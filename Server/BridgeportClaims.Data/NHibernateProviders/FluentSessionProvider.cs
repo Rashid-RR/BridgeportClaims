c76f1fa8d40179f1089fdcf5f49718694a6c88ed
Jordan Gurney <jgurney2016@hotmail.com>
2017-05-22 02:24:16 -0600

    Adding new Class Libraries

---
+++ Server/BridgeportClaims.Data/NHibernateProviders/FluentSessionProvider.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Web;
+using NHibernate;
+using NHibernate.Context;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace BridgeportClaims.Data.NHibernateProviders
+{
+    public class FluentSessionProvider : IHttpModule
+    {
+        private static ISessionFactory _sessionFactory;
+        private static readonly object Semiphore = new object();
+
+        public static ISessionFactory SessionFactory
+        {
+            get
+            {
+                lock (Semiphore)
+                {
+                    return _sessionFactory ?? (_sessionFactory = CreateSessionFactory());
+                }
+            }
+        }
+
+        static FluentSessionProvider() { }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += BeginRequest;
+            context.EndRequest += EndRequest;
+        }
+
+        public void Dispose() { }
+
+        public static ISession GetCurrentSession()
+        {
+            var factory = SessionFactory;
+            if (!CurrentSessionContext.HasBind(factory))
+                CurrentSessionContext.Bind(factory.OpenSession());
+            return factory.GetCurrentSession();
+        }
+
+        private static void BeginRequest(object sender, EventArgs e)
+        {
+            var session = SessionFactory.OpenSession();
+            session.BeginTransaction(IsolationLevel.Snapshot);
+            CurrentSessionContext.Bind(session);
+        }
+
+        private static void EndRequest(object sender, EventArgs e)
+        {
+            var session = CurrentSessionContext.Unbind(SessionFactory);
+            if (session == null) return;
+            try
+            {
+                session.Transaction.Commit();
+            }
+            catch
+            {
+                session.Transaction.Rollback();
+            }
+            finally
+            {
+                session.Close();
+                session.Dispose();
+            }
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            var timeout = TimeSpan.FromMinutes(30).TotalSeconds;
+            return Fluently.Configure()
+                .Database(CreateDbConfig)
+                .Mappings(m =>
+                {
+                    //m.FluentMappings.Add<OrdersMap>();
+                })
+                .ExposeConfiguration(c => c.SetProperty("current_session_context_class", "call"))
+                .ExposeConfiguration(c => c.SetProperty("command_timeout", timeout.ToString(CultureInfo.InvariantCulture)))
+                .CurrentSessionContext<WebSessionContext>()
+                .Cache(c => c.Not.UseSecondLevelCache())
+                .BuildSessionFactory();
+        }
+
+        private static MsSqlConfiguration CreateDbConfig()
+        {
+            return MsSqlConfiguration
+                .MsSql2012 // Actually 2014. Currently no dialect for MSSQL 2014 exists yet.
+                // .ShowSql() // Nah... Performance Impact.
+                .ConnectionString(c =>
+                    c.FromConnectionStringWithKey(
+                        "JGPortal.Services.Properties.Settings.JGPortalConnectionString"));
+        }
+    }
+}
--- Server/BridgeportClaims.Web/BridgeportClaims.Web/Global.asax.cs
+++ Server/BridgeportClaims.Web/BridgeportClaims.Web/Global.asax.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BridgeportClaims.Web
@@ -13,11 +8,9 @@ namespace BridgeportClaims.Web
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
