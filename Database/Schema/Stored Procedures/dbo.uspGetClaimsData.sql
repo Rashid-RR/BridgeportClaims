c3ce26f4b2c55a277b39e5ef5476f02dfbe23de8
Jordan Gurney <jgurney2016@hotmail.com>
2017-06-14 18:54:38 -0600

    Changes

--- Database/Schema/Stored Procedures/dbo.uspGetClaimsData.sql
+++ Database/Schema/Stored Procedures/dbo.uspGetClaimsData.sql
@@ -26,29 +26,24 @@ BEGIN
           SELECT i.ClaimID FROM dbo.Invoice i WHERE i.InvoiceNumber = @InvoiceNumber UNION
           SELECT p.ClaimID FROM dbo.Prescription p WHERE p.RxNumber = @RxNumber
     )
-    SELECT ClaimId = c.ClaimID
-         , [Name] = NULLIF(ISNULL(LTRIM(RTRIM(p.FirstName)), '') + ' ' + ISNULL(LTRIM(RTRIM(p.LastName)), ''), ' ')
+    SELECT c.ClaimId
+         , c.[Name]
          , c.ClaimNumber
-         , DateOfBirth = FORMAT(p.DateOfBirth, @Format)
-         , InjuryDate = FORMAT(c.DateOfInjury, @Format)
-         , Gender = g.GenderName
-         , Carrier = pa.BillToName -- ?
-         , Adjustor = a.AdjustorName
-         , AdjustorPhoneNumber = a.PhoneNumber
-         , DateEntered = FORMAT(c.TermDate, @Format) -- ?
-         , AdjustorFaxNumber = a.FaxNumber
-    FROM   dbo.Claim c 
-           INNER JOIN ClaimsCTE cte ON cte.ClaimID = c.ClaimID
-           LEFT JOIN dbo.Patient p INNER JOIN dbo.Gender g ON g.GenderID = p.GenderID ON p.ClaimID = c.ClaimID
-           LEFT JOIN dbo.Payor pa ON pa.PayorID = c.PayorID
-           LEFT JOIN dbo.Adjustor a ON a.AdjustorID = c.AdjusterID
-           LEFT JOIN dbo.Invoice i ON i.ClaimID = c.ClaimID
-           LEFT JOIN dbo.Prescription pre ON pre.ClaimID = c.ClaimID
+         , c.DateOfBirth
+         , InjuryDate
+         , Gender
+         , Carrier
+         , Adjustor
+         , AdjustorPhoneNumber
+         , DateEntered
+         , AdjustorFaxNumber
+    FROM   dbo.vwClaims c 
+           INNER JOIN ClaimsCTE cte ON cte.ClaimID = c.ClaimId
     WHERE  1 = 1
            AND (c.ClaimNumber = @ClaimNumber OR @ClaimNumber IS NULL)
-           AND (p.FirstName = @FirstName OR @FirstName IS NULL)
-           AND (p.LastName = @LastName OR @LastName IS NULL)
-           AND (i.InvoiceNumber = @InvoiceNumber OR @InvoiceNumber IS NULL)
-           AND (pre.RxNumber = @RxNumber OR @RxNumber IS NULL);
+           AND (c.FirstName = @FirstName OR @FirstName IS NULL)
+           AND (c.LastName = @LastName OR @LastName IS NULL)
+           AND (c.InvoiceNumber = @InvoiceNumber OR @InvoiceNumber IS NULL)
+           AND (c.RxNumber = @RxNumber OR @RxNumber IS NULL);
 END
 GO
