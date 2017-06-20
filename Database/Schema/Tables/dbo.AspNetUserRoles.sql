97dffb687ccdcc6c53f08e7035a4bf14c7ff7e72
Jordan Gurney <jordangurney@gmail.com>
2017-06-20 12:20:31 -0600

    User roles objects

--- Database/Schema/Tables/dbo.AspNetUserRoles.sql
+++ Database/Schema/Tables/dbo.AspNetUserRoles.sql
@@ -8,6 +8,28 @@ WITH
 DATA_COMPRESSION = ROW
 )
 GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+/*
+	Author:			Jordan Gurney
+	Create Date:	6/20/2017
+	Description:	Trigger designed to automatically assign a user to the "User" role,
+					if they are inserted into any other relationship to any other role.
+*/
+CREATE TRIGGER [dbo].[utAspNetUserRolesAddAllUsersToUserRole] ON [dbo].[AspNetUserRoles]
+	AFTER INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @UserID NVARCHAR(128)
+	SELECT @UserID = i.UserId FROM Inserted i
+	IF @UserID IS NULL
+		RETURN
+    EXEC dbo.uspAddUsersToUserRoleGroup @UserID
+END
+GO
 ALTER TABLE [dbo].[AspNetUserRoles] ADD CONSTRAINT [pkAspNetUserRoles] PRIMARY KEY CLUSTERED  ([UserID], [RoleID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
 GO
 CREATE NONCLUSTERED INDEX [idxAspNetUserRolesRoleID] ON [dbo].[AspNetUserRoles] ([RoleID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = PAGE) ON [PRIMARY]
