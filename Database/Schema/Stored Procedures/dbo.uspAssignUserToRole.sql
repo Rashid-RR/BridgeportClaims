9000464411c565aad3c3d61e924c16fabfe7f7a3
Jordan Gurney <jordangurney@gmail.com>
2017-06-22 12:33:22 -0600

    Some changes I may not end up needing.

--- Database/Schema/Stored Procedures/dbo.uspAssignUserToRole.sql
+++ Database/Schema/Stored Procedures/dbo.uspAssignUserToRole.sql
@@ -7,12 +7,14 @@ GO
 	Create Date:	6/21/2017
 	Description:	Inserts an "Admin" or "User" role into the database,
 					given a user's Email Address
+	Modified:		6/22/2017 - Added call to new proc "dbo.uspCleanupUserRoles"
 	Sample Execute:
 					EXEC dbo.uspAssignUserToRole 'jogwayi@gmail.com', 'Admin'
 */
 CREATE PROC [dbo].[uspAssignUserToRole] @Email NVARCHAR(256), @RoleName NVARCHAR(256)
 AS BEGIN
 	SET NOCOUNT ON
+	EXEC dbo.uspCleanupUserRoles
 	DECLARE @UserID NVARCHAR(128), @RoleID NVARCHAR(128)
 	
 	SELECT @UserID = u.ID
