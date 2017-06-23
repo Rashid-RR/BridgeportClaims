3f74b79054923169c46392faa0ad236419492ac2
Josephat <jogwayi@oosint.com>
2017-06-23 13:59:14 +0300

    Cleanup, resize claim result panel

--- Client/src/app/services/claim-manager.ts
+++ Client/src/app/services/claim-manager.ts
@@ -17,15 +17,31 @@ export class ClaimManager{
   
   search(data){    
     this.http.getClaimsData(data).map(res=>{return res.json()})
-    .subscribe((result:Array<Claim>)=>{
-        //this.claims = result;
-        this.claims = Immutable.OrderedMap<Number, Claim>();
-        result.forEach(claim=>{
-         var c = new Claim(claim.claimId,claim.claimNumber,claim.dateEntered,claim.injuryDate,claim.gender,
-         claim.carrier,claim.adjustor,claim.adjustorPhoneNumber,claim.dateEntered,claim.adjustorPhoneNumber
-         ,claim.name,claim.firstName,claim.lastName);
-         this.claims = this.claims.set(claim.claimId,c);
-        })
+    .subscribe((result:any)=>{
+          this.selected=undefined;
+        if(result.name){
+            this.claims = Immutable.OrderedMap<Number, Claim>();
+            var c = new Claim(-10,result.claimNumber,result.dateEntered,result.injuryDate,result.gender,
+            result.carrier,result.adjustor,result.adjustorPhoneNumber,result.dateEntered,result.adjustorPhoneNumber
+            ,result.name,result.firstName,result.lastName);
+            this.claims = this.claims.set(-10,c);
+            let claim = this.claims.get(-10);
+            claim.setPrescription(result.prescriptions); 
+            claim.setPayment(result.payments);
+            claim.setEpisodes(result.episodes);
+            claim.setClaimNotes(result.claimNote);
+            claim.setPrescriptionNotes(result.prescriptionNotes);
+        }else{
+            let res:  Array<Claim>  = result;
+            this.claims = Immutable.OrderedMap<Number, Claim>();
+            result.forEach(claim=>{
+            var c = new Claim(claim.claimId,claim.claimNumber,claim.dateEntered,claim.injuryDate,claim.gender,
+            claim.carrier,claim.adjustor,claim.adjustorPhoneNumber,claim.dateEntered,claim.adjustorPhoneNumber
+            ,claim.name,claim.firstName,claim.lastName);
+            this.claims = this.claims.set(claim.claimId,c);
+            })
+        }
+        //this.claims = result;        
         //console.log(result);
       },err=>{
         console.log(err);
@@ -42,16 +58,18 @@ export class ClaimManager{
   getClaimsDataById(id:Number){
       this.selected = id;
       var claim:Claim = this.claims.get(id) as Claim; 
-      this.http.getClaimsData({claimId:id}).map(res=>{return res.json()})
-        .subscribe(result=>{
-            claim.setPrescription(result.prescriptions); 
-            claim.setPayment(result.payments);
-            claim.setEpisodes(result.episodes);
-            claim.setClaimNotes(result.claimNote);
-            claim.setPrescriptionNotes(result.prescriptionNotes);
-        },err=>{
-          console.log(err);
-        })
+      if(id !== -10){  
+        this.http.getClaimsData({claimId:id}).map(res=>{return res.json()})
+          .subscribe(result=>{
+              claim.setPrescription(result.prescriptions); 
+              claim.setPayment(result.payments);
+              claim.setEpisodes(result.episodes);
+              claim.setClaimNotes(result.claimNote);
+              claim.setPrescriptionNotes(result.prescriptionNotes);
+          },err=>{
+            console.log(err);
+          })
+      }
   }
   get selectedClaim():Claim{
     return this.claims.get(this.selected)
