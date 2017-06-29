16e929a887ab05c66d9fe3f3582a547ad371bafc
Jordan Gurney <jordangurney@gmail.com>
2017-06-29 00:33:55 -0600

    ClaimNoteTypes

---
+++ Database/Schema/Data/dbo.ClaimNoteType_Data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[ClaimNoteType] ON
+INSERT INTO [dbo].[ClaimNoteType] ([ClaimNoteTypeID], [TypeName], [Code], [CreatedOn], [UpdatedOn]) VALUES (1, 'Initial Discovery', 'ID', '2017-06-27 11:47:37.1368341', '2017-06-27 11:47:37.1368341')
+INSERT INTO [dbo].[ClaimNoteType] ([ClaimNoteTypeID], [TypeName], [Code], [CreatedOn], [UpdatedOn]) VALUES (2, 'Underwriting Approval', 'UA', '2017-06-27 11:47:37.1368341', '2017-06-27 11:47:37.1368341')
+INSERT INTO [dbo].[ClaimNoteType] ([ClaimNoteTypeID], [TypeName], [Code], [CreatedOn], [UpdatedOn]) VALUES (3, 'Claim Approved', 'CA', '2017-06-27 11:47:37.1368341', '2017-06-27 11:47:37.1368341')
+INSERT INTO [dbo].[ClaimNoteType] ([ClaimNoteTypeID], [TypeName], [Code], [CreatedOn], [UpdatedOn]) VALUES (4, 'Claim Denied', 'CD', '2017-06-27 11:47:37.1368341', '2017-06-27 11:47:37.1368341')
+SET IDENTITY_INSERT [dbo].[ClaimNoteType] OFF
--- Database/Schema/Security/Users/bridgeportClaimsUser.sql
+++ Database/Schema/Security/Users/bridgeportClaimsUser.sql
@@ -1,9 +1,2 @@
 CREATE USER [bridgeportClaimsUser] WITHOUT LOGIN
 GO
-GRANT CHECKPOINT TO [bridgeportClaimsUser]
-GRANT DELETE TO [bridgeportClaimsUser]
-GRANT EXECUTE TO [bridgeportClaimsUser]
-GRANT INSERT TO [bridgeportClaimsUser]
-GRANT REFERENCES TO [bridgeportClaimsUser]
-GRANT SELECT TO [bridgeportClaimsUser]
-GRANT UPDATE TO [bridgeportClaimsUser]
