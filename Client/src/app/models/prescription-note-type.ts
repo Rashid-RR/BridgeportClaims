01400c64de12ff4f600695e4647ccfbba6fa5ca9
Josephat <jogwayi@oosint.com>
2017-07-04 16:55:38 +0300

    Prescription Note UI and service integratio

---
+++ Client/src/app/models/prescription-note-type.ts
@@ -0,0 +1,8 @@
+export class PrescriptionNoteType{
+    prescriptionNoteTypeId:String;
+    typeName:  Number
+    constructor(prescriptionNoteTypeId:String,typeName:any){
+        this.prescriptionNoteTypeId = prescriptionNoteTypeId; 
+        this.typeName = typeName;
+    }
+}
--- Client/src/app/pages/claim/claim.component.ts
+++ Client/src/app/pages/claim/claim.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpService} from "../../services/http-service"
 import {ClaimManager} from "../../services/claim-manager";
+import {PrescriptionNoteType} from "../../models/prescription-note-type";
+import swal from "sweetalert2";
+import {warn,success} from "../../models/notification"
 
 @Component({
   selector: 'app-claim',
@@ -11,6 +14,7 @@ export class ClaimsComponent implements OnInit {
   
   expanded:Boolean=false
   expandedBlade:Number=0;
+  
   constructor(public claimManager:ClaimManager,private http:HttpService) {
      
    }
@@ -25,6 +29,85 @@ export class ClaimsComponent implements OnInit {
   }
   ngOnInit() {
      window['jQuery']('body').addClass('sidebar-collapse');
+  }
+
+  addPrescriptionNote(text:String="",TypeId?:String,prescriptionNoteId:any=null){
+    let selectedNotes = [];
+    let prescriptionNoteTypeIds = '<option value="" style="color:purple">Select type</option>';
+      this.claimManager.PrescriptionNoteTypes.forEach((note:PrescriptionNoteType)=>{
+          prescriptionNoteTypeIds=prescriptionNoteTypeIds+'<option value="'+note.prescriptionNoteTypeId+'"' +(note.prescriptionNoteTypeId ==TypeId ? "selected": "")+'>'+note.typeName+'</option>';
+          selectedNotes.push(note.prescriptionNoteTypeId);
+      });
+    var selectedPrecriptions = '';
+     this.claimManager.selectedClaim.prescriptions.forEach(prescription=>{
+        if(prescription.selected){
+            selectedPrecriptions = selectedPrecriptions + '<span class="label label-info"  style="margin:2px;display:inline-flex">'+prescription.labelName+'</span> &nbsp; ';
+        }
+    });
+     
+    swal({
+      title: 'New Prescription Note',
+      html:
+        `
+              <div class="form-group">
+                  <label id="prescriptionNoteTypeLabel">Prescription Note type</label>
+                  <select class="form-control" id="prescriptionNoteTypeId">
+                    `+prescriptionNoteTypeIds+`
+                  </select>
+              </div>
+              <div class="form-group">
+                  <label id="noteTextLabel">Note Text</label>
+                  <textarea class="form-control"  id="noteText" rows="3">`+text+`</textarea>
+              </div>
+              <div style="text-align:left">
+                  `+selectedPrecriptions+`              
+              </div>
+        `,
+      showCancelButton: true,
+      showLoaderOnConfirm:true,
+      confirmButtonText:"Save",
+      preConfirm: function () {
+        return new Promise(function (resolve) {
+          resolve([
+            window['jQuery']('#prescriptionNoteTypeId').val(),
+            window['jQuery']('#noteText').val()
+          ])
+        })
+      },
+      onOpen: function () {
+        window['jQuery']('#prescriptionNoteTypeId').focus()
+      }
+    }).then( (result)=> {
+      if(result[0]==""){
+          warn('Please select one type!');
+          setTimeout(()=>{
+              this.addPrescriptionNote(result[1],result[0]);
+              window['jQuery']('#prescriptionNoteTypeLabel').css({"color":"red"})
+            },200)
+      }else if(result[1]==""){
+          warn('Note Text is required!');
+          setTimeout(()=>{
+            this.addPrescriptionNote(result[1],result[0]);
+            window['jQuery']('#noteTextLabel').css({"color":"red"})
+          },200)
+      }else{
+          swal({title:"",html:"Saving note... <br/> <i class='fa fa-refresh fa-2x fa-spin'></i>",showConfirmButton:false})
+          this.http.savePrescriptionNote(
+            {
+              "claimId": this.claimManager.selectedClaim.claimId,
+              "noteText": result[1],
+              "prescriptionNoteTypeId": result[0],
+              "Prescriptions": selectedNotes,
+              "prescriptionNoteId": prescriptionNoteId
+            }).single().subscribe(result=>{
+              console.log(result);
+              swal.clickConfirm();
+              success("Noted successfully saved");
+            },error=>{
+              console.log("Saving error",error);
+            })
+      } 
+    }).catch(swal.noop)
   }
 
 }
