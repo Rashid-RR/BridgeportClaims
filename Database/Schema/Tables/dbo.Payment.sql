3ecc8c7c45b8d1106a2fbfb69fc05fdb6ca78f68
Jordan Gurney <jordangurney@gmail.com>
2017-07-27 04:06:05 -0600

    Payments System

--- Database/Schema/Tables/dbo.Payment.sql
+++ Database/Schema/Tables/dbo.Payment.sql
@@ -1,15 +1,11 @@
 CREATE TABLE [dbo].[Payment]
 (
 [PaymentID] [int] NOT NULL IDENTITY(1, 1),
-[CheckNumber] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[CheckDate] [date] NOT NULL,
+[CheckNumber] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [AmountPaid] [money] NOT NULL,
-[ClaimID] [int] NOT NULL,
-[InvoiceID] [int] NOT NULL,
-[CreatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfPaymentCreatedOnUTC] DEFAULT (sysutcdatetime()),
-[UpdatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfPaymentUpdatedOnUTC] DEFAULT (sysutcdatetime()),
-[DataVersion] [timestamp] NOT NULL,
-[ETLRowID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[DateScanned] [date] NULL,
+[PrescriptionID] [int] NULL,
+[ClaimID] [int] NOT NULL
 ) ON [PRIMARY]
 WITH
 (
@@ -18,9 +14,7 @@ DATA_COMPRESSION = ROW
 GO
 ALTER TABLE [dbo].[Payment] ADD CONSTRAINT [pkPayment] PRIMARY KEY CLUSTERED  ([PaymentID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
 GO
-CREATE NONCLUSTERED INDEX [idxPaymentClaimIDIncludeAll] ON [dbo].[Payment] ([ClaimID]) INCLUDE ([AmountPaid], [CheckDate], [CheckNumber], [CreatedOnUTC], [InvoiceID], [PaymentID], [UpdatedOnUTC]) WITH (FILLFACTOR=90, DATA_COMPRESSION = PAGE) ON [PRIMARY]
-GO
 ALTER TABLE [dbo].[Payment] ADD CONSTRAINT [fkPaymentClaimIDClaimClaimID] FOREIGN KEY ([ClaimID]) REFERENCES [dbo].[Claim] ([ClaimID])
 GO
-ALTER TABLE [dbo].[Payment] ADD CONSTRAINT [fkPaymentInvoiceIDInvoiceInvoiceID] FOREIGN KEY ([InvoiceID]) REFERENCES [dbo].[Invoice] ([InvoiceID])
+ALTER TABLE [dbo].[Payment] ADD CONSTRAINT [fkPaymentPrescriptionIDPrescriptionPrescriptionID] FOREIGN KEY ([PrescriptionID]) REFERENCES [dbo].[Prescription] ([PrescriptionID])
 GO
