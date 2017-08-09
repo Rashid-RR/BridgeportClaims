64efd2464889c5acb91cd043648631107d1f8599
Csaba <todorcs@gmail.com>
2017-08-09 15:23:10 +0300

    Added Total Results found for Claims Search - TASK#117

--- Client/src/app/components/claim-result/claim-result.component.ts
+++ Client/src/app/components/claim-result/claim-result.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit,Input } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {ClaimManager} from "../../services/claim-manager";
-import {EventsService} from "../../services/events-service";
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ClaimManager } from "../../services/claim-manager";
+import { EventsService } from "../../services/events-service";
 
 @Component({
   selector: 'app-claim-result',
@@ -11,19 +11,27 @@ import {EventsService} from "../../services/events-service";
 })
 export class ClaimResultComponent implements OnInit {
 
-  @Input() expand:Function;
-  @Input() minimize:Function;
-  constructor(public claimManager:ClaimManager,private formBuilder: FormBuilder, private router: Router, private events: EventsService) {
-    
-  }
+  @Input() expand: Function;
+  @Input() minimize: Function;
+
+
+
+  constructor(
+    public claimManager: ClaimManager,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private events: EventsService
+  ) { }
 
   ngOnInit() {
-    
+    let claimsLength = this.claimManager.claimsData;
+    console.log(claimsLength.length);
+
   }
 
-  view(claimID:Number){
-    this.claimManager.getClaimsDataById(claimID);   
-    this.events.broadcast('minimize',[]); 
+  view(claimID: Number) {
+    this.claimManager.getClaimsDataById(claimID);
+    this.events.broadcast('minimize', []);
     //this.minimize();
   }
 
