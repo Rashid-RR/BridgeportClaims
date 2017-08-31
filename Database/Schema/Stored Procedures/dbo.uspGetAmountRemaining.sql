6f9e70e2c7c25c7da110f2f9b2aa487ef0ed628e
Jordan Gurney <jordangurney@gmail.com>
2017-08-31 02:49:35 -0600

    Stuff

--- Database/Schema/Stored Procedures/dbo.uspGetAmountRemaining.sql
+++ Database/Schema/Stored Procedures/dbo.uspGetAmountRemaining.sql
@@ -7,9 +7,11 @@ GO
 	Create Date:	8/30/2017
 	Description:	Returns the Check #, Check Amount, and Amount Remaining
 	Sample Execute:
+					DECLARE @AmountRemaining MONEY
 					DECLARE @Base [dbo].[udtClaimID] INSERT @Base (ClaimID) 
 					VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9)
-					EXEC dbo.uspGetAmountRemaining @Base, '99999'
+					EXEC dbo.uspGetAmountRemaining @Base, '99999', @AmountRemaining OUTPUT
+					SELECT @AmountRemaining
 */
 CREATE PROC [dbo].[uspGetAmountRemaining](@ClaimIDs [dbo].[udtClaimID] READONLY,
 @CheckNumber VARCHAR(50),
@@ -19,16 +21,19 @@ AS BEGIN
 
     DECLARE @SplitList VARCHAR(8000)
 
-    SELECT @SplitList=COALESCE(@SplitList+',', '')+CAST(p.ClaimID AS VARCHAR)
+    SELECT @SplitList = COALESCE(@SplitList+',', '') + CAST(p.ClaimID AS VARCHAR)
     FROM @ClaimIDs AS p
 
     CREATE TABLE #PossibleClaimIDs (ClaimID INT) -- Needs to match at least one of these.
 
-    INSERT #PossibleClaimIDs(ClaimID)
-    SELECT Split.value FROM STRING_SPLIT(@SplitList, ',') AS Split
+    INSERT #PossibleClaimIDs (ClaimID)
+    SELECT	Split.[value]
+	FROM	STRING_SPLIT(@SplitList, ',') AS Split
 
-    SELECT @AmountRemaining=SUM(p.AmountPaid)
+    SELECT @AmountRemaining = SUM(p.AmountPaid)
     FROM dbo.vwPayment AS p
-    WHERE p.CheckNumber=@CheckNumber AND p.ClaimID IN(SELECT c.ClaimID FROM #PossibleClaimIDs AS c)
+    WHERE p.CheckNumber = @CheckNumber 
+		-- Claim ID must exist somewhere in this table.
+		AND p.ClaimID IN (SELECT c.ClaimID FROM #PossibleClaimIDs AS c)
 END
 GO
