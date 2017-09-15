2814ffc94846ae68ccc2f283afa376af98e7a46d
Jordan Gurney <jordangurney@gmail.com>
2017-09-15 03:45:04 -0600

    Payments posting - To Suspense button.

--- Server/BridgeportClaims.Web/Models/PaymentPosting.cs
+++ Server/BridgeportClaims.Web/Models/PaymentPosting.cs
@@ -7,6 +7,8 @@ namespace BridgeportClaims.Web.Models
         public string PatientName { get; set; }
         public DateTime RxDate { get; set; }
         public decimal AmountPosted { get; set; }
+        public decimal InvoiceAmount { get; set; }
+        public decimal Outstanding => InvoiceAmount - AmountPosted;
         public int PrescriptionId { get; set; }
     }
 }
