f829ac50c54b0335cd98620fa445dde3968191ba
Jordan Gurney <jordangurney@gmail.com>
2017-09-16 02:47:34 -0600

    Committing changes originally developed in the Payments branch to populate a "Payments" blade grid.

--- Server/DAL/BridgeportClaims.Data/DataProviders/Payments/IPaymentsDataProvider.cs
+++ Server/DAL/BridgeportClaims.Data/DataProviders/Payments/IPaymentsDataProvider.cs
@@ -8,6 +8,8 @@ namespace BridgeportClaims.Data.DataProviders.Payments
     {
         decimal GetAmountRemaining(IList<int> claimsIds, string checkNumber);
         void ImportPaymentFile(string fileName);
+        IList<PrescriptionPaymentsDto> GetPrescriptionPaymentsDtos(int claimId, string sortColumn,
+            string direction, int pageNumber, int pageSize, string secondarySortColumn, string secondaryDirection);
         IList<ClaimsWithPrescriptionDetailsDto> GetClaimsWithPrescriptionDetails(IList<int> claimIds);
         IList<ClaimsWithPrescriptionCountsDto> GetClaimsWithPrescriptionCounts(string claimNumber, string firstName,
             string lastName, DateTime? rxDate, string invoiceNumber);
