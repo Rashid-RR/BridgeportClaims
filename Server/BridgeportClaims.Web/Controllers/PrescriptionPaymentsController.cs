4023d08f35470587dba0e2d2b975de7216e6f75f
Jordan Gurney <jordangurney@gmail.com>
2017-09-17 03:36:27 -0600

    committing code changes

---
+++ Server/BridgeportClaims.Web/Controllers/PrescriptionPaymentsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BridgeportClaims.Data.DataProviders.PrescriptionPayments;
+using BridgeportClaims.Web.Models;
+using Microsoft.AspNet.Identity;
+using NLog;
+
+namespace BridgeportClaims.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/prescription-payments")]
+    public class PrescriptionPaymentsController : BaseApiController
+    {
+        private readonly IPrescriptionPaymentProvider _provider;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public PrescriptionPaymentsController(IPrescriptionPaymentProvider provider)
+        {
+            _provider = provider;
+        }
+
+        [HttpPost]
+        [Route("delete")]
+        public async Task<IHttpActionResult> DeletePrescriptionPayment(int prescriptionPaymentId)
+        {
+            try
+            {
+                return await Task.Run(() =>
+                {
+                    _provider.DeletePrescriptionPayment(prescriptionPaymentId);
+                    return Ok(new {message = "The prescription payment was deleted successfully."});
+                });
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return Content(HttpStatusCode.InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public async Task<IHttpActionResult> UpdatePrescriptionPayment(PrescriptionPaymentViewModel model)
+        {
+            try
+            {
+                return await Task.Run(() =>
+                {
+                    var userId = User.Identity.GetUserId();
+                    _provider.UpdatePrescriptionPayment(model.PrescriptionPaymentId, model.CheckNumber, model.AmountPaid,
+                        model.DatePosted, model.PrescriptionId, userId);
+                    return Ok(new { message = "The prescription payment was updated successfully." });
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
