e126c5bf8a20a0acb8d9d52e0211705af3928b34
Josephat <jogwayi@oosint.com>
2017-10-09 14:17:47 +0300

    references #314

--- Client/src/app/models/prescription-notes.ts
+++ Client/src/app/models/prescription-notes.ts
@@ -3,11 +3,15 @@ export class PrescriptionNotes{
     enteredBy:String;
     type:String;
     note:String;
-    date:Date;
-      constructor(date:Date,enteredBy:String,type:String,note:String){
-          this.date = date;
+    noteUpdatedOn:Date;
+    rxDate:Date;
+    rxNumber:Number;
+      constructor(noteUpdatedOn:Date,enteredBy:String,type:String,note:String,rxDate:Date,rxNumber:Number){
+          this.noteUpdatedOn = noteUpdatedOn;
           this.type = type;
           this.enteredBy = enteredBy;
           this.note = note;
+          this.rxDate = rxDate;
+          this.rxNumber = rxNumber;
       }
 }
