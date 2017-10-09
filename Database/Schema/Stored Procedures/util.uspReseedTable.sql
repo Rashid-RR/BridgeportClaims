6070af23fee9419ec04da5f66f4843f5575f5587
Jordan Gurney <jordangurney@gmail.com>
2017-10-09 11:20:10 -0600

    Reseed proc

--- Database/Schema/Stored Procedures/util.uspReseedTable.sql
+++ Database/Schema/Stored Procedures/util.uspReseedTable.sql
@@ -4,13 +4,18 @@ SET ANSI_NULLS ON
 GO
 /*
 	Author:			Jordan Gurney
-	Create Date:	8/21/2017
-	Description:	Finds the last ID used and reseeds table to that value.
+	Create Date:	8/21/2015
+	Description:	Runs a DBCC CHECKIDENT to reseed a table to the last PK value used,
+					or to the value explicitly passed in.
 	Sample Execute:
-					EXEC util.uspReseedTable 'dbo.Payment'
+					EXEC util.uspReseedTable 'schema_name.table_name', 50000
 */
-CREATE PROC [util].[uspReseedTable] @TableName SYSNAME, -- Include schema name.
+CREATE PROC [util].[uspReseedTable]
+(
+	@TableName SYSNAME, -- Include schema name.
+	@SeedValue INT = NULL, -- Optional param to specify Seed Value. This is where new rows will start.
 	@DebugOnly BIT = 0
+)
 AS BEGIN
 	SET NOCOUNT ON;
 	IF (@TableName LIKE '%]%' OR @TableName LIKE '%[%')
@@ -20,8 +25,10 @@ AS BEGIN
 			@TableNameOnly SYSNAME = PARSENAME(@TableName, 1)
 	DECLARE @FullyQualifiedName SYSNAME = QUOTENAME(@TableSchema) + '.' + QUOTENAME(@TableNameOnly)
 	SET @SQLStatement =
-		N'DECLARE @MaxID INT; SELECT @MaxID = MAX(' + util.udfGetPrimaryKeyColumnName(@FullyQualifiedName) +
-		 N') FROM ' + @FullyQualifiedName
+		N'DECLARE @MaxID INT; SELECT @MaxID = ' + 
+		CASE WHEN @SeedValue IS NULL THEN
+		'MAX(' + util.udfGetPrimaryKeyColumnName(@FullyQualifiedName) +
+		 N') FROM ' + @FullyQualifiedName ELSE CONVERT(NVARCHAR, (@SeedValue - 1)) END;
 	SET @SQLStatement += N'; IF @MaxID IS NOT NULL BEGIN DBCC CHECKIDENT(''' +
 		@FullyQualifiedName + N''', RESEED, @MaxID) END;'
 	IF @DebugOnly = 1
