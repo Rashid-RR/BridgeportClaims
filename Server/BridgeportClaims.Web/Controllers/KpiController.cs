816d5eba4877039df6edf70bf8e72fe060027cc2
Jordan Gurney <jordangurney@gmail.com>
2017-10-16 06:51:54 -0600

    committing staged KPI Changes

---
+++ Server/BridgeportClaims.Web/Controllers/KpiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BridgeportClaims.Data.DataProviders.KPI;
+using NLog;
+
+namespace BridgeportClaims.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/kpi")]
+    public class KpiController : BaseApiController
+    {
+        private readonly IKpiProvider _kpiProvider;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public KpiController(IKpiProvider kpiProvider)
+        {
+            _kpiProvider = kpiProvider;
+        }
+
+        [HttpPost]
+        [Route("revenue")]
+        public async Task<IHttpActionResult> GetMonthlyRevenue()
+        {
+            try
+            {
+                return await Task.Run(() =>
+                {
+                    var month = DateTime.Now.Month;
+                    var year = DateTime.Now.Year;
+                    var results = _kpiProvider.GetPaymentTotalsDtos(month, year);
+                    return Ok(results);
+                });
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return Content(HttpStatusCode.NotAcceptable, new {message = ex.Message});
+            }
+        }
+    }
+}
