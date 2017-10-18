7c595cf60efe3988feb5e478e653bef618ddeea9
Josephat <jogwayi@oosint.com>
2017-10-18 10:56:43 +0300

    references #401

--- Client/src/app/pages/dashboard-links/dashboard-links.component.ts
+++ Client/src/app/pages/dashboard-links/dashboard-links.component.ts
@@ -22,77 +22,79 @@ export class DashboardLinksComponent implements OnInit ,AfterViewInit{
   ) { }
 
   ngAfterViewInit() { 
-    this.http.revenueByMonth({}).map(r=>{return r.json()}).subscribe((res:Array<{datePosted:Date,totalPosted:Number}>)=>{
-      let drilldown:any=[];
-      res.forEach(r=>{
-        let date = this.dp.transform(r.datePosted, "M/d");
-        this.categories.push(date);
-        /* 
-        For testing
-        if(r.totalPosted==363872.7){
-          r.totalPosted=1917;
-        } */
-        this.data.push({
-          name:date,
-          drilldown:date,
-          y:r.totalPosted
-        });
-        drilldown.push({
-          name:date,
-          id:date,
-          data:[[r.totalPosted]]
-        });
-      })
-      var chart = Highcharts.chart('container', {      
-          title: {
-              text: 'Revenue By Month'
-          },
-          chart: {
-            type: 'column'
-          },
-          credits: {
-            enabled: false
-          },
-          plotOptions: {
-              series: {
-                  borderWidth: 0,
-                  dataLabels: {
-                      enabled: true,
-                      format: '${point.y:.2f}'
-                  }
+    if(this.allowed){
+      this.http.revenueByMonth({}).map(r=>{return r.json()}).subscribe((res:Array<{datePosted:Date,totalPosted:Number}>)=>{
+        let drilldown:any=[];
+        res.forEach(r=>{
+          let date = this.dp.transform(r.datePosted, "M/d");
+          this.categories.push(date);
+          /* 
+          For testing
+          if(r.totalPosted==363872.7){
+            r.totalPosted=1917;
+          } */
+          this.data.push({
+            name:date,
+            drilldown:date,
+            y:r.totalPosted
+          });
+          drilldown.push({
+            name:date,
+            id:date,
+            data:[[r.totalPosted]]
+          });
+        })
+        var chart = Highcharts.chart('container', {      
+            title: {
+                text: 'Revenue By Month'
+            },
+            chart: {
+              type: 'column'
+            },
+            credits: {
+              enabled: false
+            },
+            plotOptions: {
+                series: {
+                    borderWidth: 0,
+                    dataLabels: {
+                        enabled: true,
+                        format: '${point.y:.2f}'
+                    }
+                }
+            },
+            yAxis: [{
+                title: {
+                    text: 'Totals Posted'
+                }
+            }],
+            xAxis: {
+                categories: this.categories,
+                title: {
+                  text: 'Date'
               }
+            },
+            legend: {
+              enabled: false
+            },
+            tooltip: {
+              headerFormat: '',
+              pointFormat: '<span style="color:{point.color}">Amount</span>: <b>${point.y:.2f}</b><br/>'
           },
-          yAxis: [{
-               title: {
-                  text: 'Totals Posted'
-              }
-          }],
-          xAxis: {
-              categories: this.categories,
-              title: {
-                text: 'Date'
+            series: [{
+                type: 'column',
+                name: 'Revenue by Month',
+                colorByPoint: true,
+                data: this.data,
+            }],
+            drilldown:{
+              series:drilldown     
             }
-          },
-          legend: {
-            enabled: false
-          },
-          tooltip: {
-            headerFormat: '',
-            pointFormat: '<span style="color:{point.color}">Amount</span>: <b>${point.y:.2f}</b><br/>'
-        },
-          series: [{
-              type: 'column',
-              name: 'Revenue by Month',
-              colorByPoint: true,
-              data: this.data,
-          }],
-          drilldown:{
-            series:drilldown     
-          }
-      });
-    },error=>{
-        
-     })
+        });
+      },error=>{
+          
+      })
+    }
   }
   ngOnInit() { 
      
