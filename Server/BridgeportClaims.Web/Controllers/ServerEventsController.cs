388e44166886e5fd18cfdd715220e480754012c8
Jordan Gurney <jordangurney@gmail.com>
2017-10-25 14:35:24 -0600

    Some needed refactoring

--- Server/BridgeportClaims.Web/Controllers/ServerEventsController.cs
+++ Server/BridgeportClaims.Web/Controllers/ServerEventsController.cs
@@ -7,39 +7,41 @@ using BridgeportClaims.Data.DataProviders.Payments;
 
 namespace BridgeportClaims.Web.Controllers
 {
-	[Authorize(Roles = "Admin")]
-	[RoutePrefix("api/ServerEvents")]
-	public class ServerEventsController : BaseApiController
-	{
-		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-		private readonly IPaymentsDataProvider _paymentsDataProvider;
-	    private readonly IImportFileProvider _importFileProvider;
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/ServerEvents")]
+    public class ServerEventsController : BaseApiController
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private readonly IPaymentsDataProvider _paymentsDataProvider;
+        private readonly IImportFileProvider _importFileProvider;
 
-		public ServerEventsController(IPaymentsDataProvider paymentsDataProvider, IImportFileProvider importFileProvider)
-		{
-		    _paymentsDataProvider = paymentsDataProvider;
-		    _importFileProvider = importFileProvider;
-		}
+        public ServerEventsController(IPaymentsDataProvider paymentsDataProvider,
+            IImportFileProvider importFileProvider)
+        {
+            _paymentsDataProvider = paymentsDataProvider;
+            _importFileProvider = importFileProvider;
+        }
 
-		[HttpPost]
-		[Route("ImportPaymentFile")]
-		public async Task<IHttpActionResult> ImportPaymentFile(string fileName)
-		{
-			return await Task.Run(() =>
-			{
-				try
-				{
-					_paymentsDataProvider.ImportPaymentFile(fileName);
-				    _importFileProvider.MarkFileProcessed(fileName);
+        [HttpPost]
+        [Route("ImportPaymentFile")]
+        public async Task<IHttpActionResult> ImportPaymentFile(string fileName)
+        {
 
+            try
+            {
+                return await Task.Run(() =>
+                {
+                    _paymentsDataProvider.ImportPaymentFile(fileName);
+                    _importFileProvider.MarkFileProcessed(fileName);
                     return Ok(new {message = "The Payment File was Processed Successfully"});
-				}
-				catch (Exception ex)
-				{
-					Logger.Error(ex);
-					throw;
-				}
-			});
-		}
-	}
+                });
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                throw;
+            }
+
+        }
+    }
 }
