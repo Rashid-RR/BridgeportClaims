2ad40febef684455c0a5794389fe3d6505eb0026
Jordan Gurney <jordangurney@gmail.com>
2017-11-06 01:21:09 -0700

    New Diaries API total row count.

--- Server/DAL/BridgeportClaims.Data/DataProviders/Diaries/DiaryProvider.cs
+++ Server/DAL/BridgeportClaims.Data/DataProviders/Diaries/DiaryProvider.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
+using BridgeportClaims.Common.Disposable;
 using BridgeportClaims.Data.Dtos;
 using BridgeportClaims.Data.StoredProcedureExecutors;
+using cs = BridgeportClaims.Common.Config.ConfigService;
 
 namespace BridgeportClaims.Data.DataProviders.Diaries
 {
@@ -17,16 +18,121 @@ namespace BridgeportClaims.Data.DataProviders.Diaries
             _storedProcedureExecutor = storedProcedureExecutor;
         }
 
-        public IList<DiariesDto> GetDiaries(bool isDefaultSort, DateTime? startDate, DateTime? endDate,
+        public DiariesDto GetDiaries(bool isDefaultSort, DateTime? startDate, DateTime? endDate,
             string sortColumn, string sortDirection, int pageNumber, int pageSize, bool closed)
-        {
-            var isDefaultSortParam = new SqlParameter
+            => DisposableService.Using(() => new SqlConnection(cs.GetDbConnStr()), conn =>
             {
-                ParameterName = "IsDefaultSort",
-                Value = isDefaultSort,
-                DbType = DbType.Boolean
-            };
-            var startDateParam = new SqlParameter
+                return DisposableService.Using(() => new SqlCommand("[dbo].[uspGetDiaries]", conn), cmd =>
+                {
+                    var isDefaultSortParam = cmd.CreateParameter();
+                    isDefaultSortParam.Direction = ParameterDirection.Input;
+                    isDefaultSortParam.DbType = DbType.Boolean;
+                    isDefaultSortParam.SqlDbType = SqlDbType.Bit;
+                    isDefaultSortParam.ParameterName = "@IsDefaultSort";
+                    isDefaultSortParam.Value = isDefaultSort;
+                    cmd.Parameters.Add(isDefaultSortParam);
+                    var startDateParam = cmd.CreateParameter();
+                    startDateParam.Direction = ParameterDirection.Input;
+                    startDateParam.DbType = DbType.Date;
+                    startDateParam.SqlDbType = SqlDbType.Date;
+                    startDateParam.ParameterName = "@StartDate";
+                    startDateParam.Value = startDate;
+                    cmd.Parameters.Add(startDateParam);
+                    var endDateParam = cmd.CreateParameter();
+                    endDateParam.Value = endDate;
+                    endDateParam.ParameterName = "@EndDate";
+                    endDateParam.DbType = DbType.Date;
+                    endDateParam.SqlDbType = SqlDbType.Date;
+                    endDateParam.Direction = ParameterDirection.Input;
+                    cmd.Parameters.Add(endDateParam);
+                    var sortParam = cmd.CreateParameter();
+                    sortParam.ParameterName = "@SortColumn";
+                    sortParam.DbType = DbType.String;
+                    sortParam.Value = sortColumn;
+                    sortParam.Direction = ParameterDirection.Input;
+                    sortParam.SqlDbType = SqlDbType.NVarChar;
+                    cmd.Parameters.Add(sortParam);
+                    var sortDirectionParam = cmd.CreateParameter();
+                    sortDirectionParam.ParameterName = "@SortDirection";
+                    sortDirectionParam.DbType = DbType.String;
+                    sortDirectionParam.Value = sortDirection;
+                    sortDirectionParam.SqlDbType = SqlDbType.VarChar;
+                    sortDirectionParam.Direction = ParameterDirection.Input;
+                    cmd.Parameters.Add(sortDirectionParam);
+                    var pageParam = cmd.CreateParameter();
+                    pageParam.ParameterName = "@PageNumber";
+                    pageParam.DbType = DbType.Int32;
+                    pageParam.Value = pageNumber;
+                    pageParam.Direction = ParameterDirection.Input;
+                    pageParam.SqlDbType = SqlDbType.Int;
+                    cmd.Parameters.Add(pageParam);
+                    var pageSizeParam = cmd.CreateParameter();
+                    pageSizeParam.ParameterName = "@PageSize";
+                    pageSizeParam.Value = pageSize;
+                    pageSizeParam.DbType = DbType.Int32;
+                    pageSizeParam.SqlDbType = SqlDbType.Int;
+                    pageSizeParam.Direction = ParameterDirection.Input;
+                    cmd.Parameters.Add(pageSizeParam);
+                    var closedParam = cmd.CreateParameter();
+                    closedParam.Direction = ParameterDirection.Input;
+                    closedParam.Value = closed;
+                    closedParam.ParameterName = "@Closed";
+                    closedParam.DbType = DbType.Boolean;
+                    closedParam.SqlDbType = SqlDbType.Bit;
+                    cmd.Parameters.Add(closedParam);
+                    var totalRowsParam = cmd.CreateParameter();
+                    totalRowsParam.ParameterName = "@TotalRows";
+                    totalRowsParam.DbType = DbType.Int32;
+                    totalRowsParam.SqlDbType = SqlDbType.Int;
+                    totalRowsParam.Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add(totalRowsParam);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    var retVal = new DiariesDto();
+                    IList<DiaryResultDto> retValResults = new List<DiaryResultDto>();
+                    if (conn.State != ConnectionState.Open)
+                        conn.Open();
+                    DisposableService.Using(cmd.ExecuteReader, reader =>
+                    {
+                        var diaryIdOrdinal = reader.GetOrdinal("DiaryId");
+                        var claimIdOrdinal = reader.GetOrdinal("ClaimId");
+                        var prescriptionNoteIdOrdinal = reader.GetOrdinal("PrescriptionId");
+                        var ownerOrdinal = reader.GetOrdinal("Owner");
+                        var createdOrdinal = reader.GetOrdinal("Created");
+                        var followUpDateOrdinal = reader.GetOrdinal("FollowUpDate");
+                        var patientNameOrdinal = reader.GetOrdinal("PatientName");
+                        var claimNumberOrdinal = reader.GetOrdinal("ClaimNumber");
+                        var typeOrdinal = reader.GetOrdinal("Type");
+                        var rxNumberOrdinal = reader.GetOrdinal("RxNumber");
+                        var rxDateOrdinal = reader.GetOrdinal("RxDate");
+                        var insuranceCarrierOrdinal = reader.GetOrdinal("InsuranceCarrier");
+                        var diaryNoteOrdinal = reader.GetOrdinal("DiaryNote");
+                        while (reader.Read())
+                        {
+                            var record = new DiaryResultDto
+                            {
+                                DiaryId = reader.GetInt32(diaryIdOrdinal),
+                                ClaimId = reader.GetInt32(claimIdOrdinal),
+                                PrescriptionNoteId = reader.GetInt32(prescriptionNoteIdOrdinal),
+                                Owner = !reader.IsDBNull(ownerOrdinal) ? reader.GetString(ownerOrdinal) : string.Empty,
+                                Created = reader.GetDateTime(createdOrdinal),
+                                FollowUpDate = reader.GetDateTime(followUpDateOrdinal),
+                                PatientName = !reader.IsDBNull(patientNameOrdinal) ? reader.GetString(patientNameOrdinal) : string.Empty,
+                                ClaimNumber = !reader.IsDBNull(claimNumberOrdinal) ? reader.GetString(claimNumberOrdinal) : string.Empty,
+                                Type = !reader.IsDBNull(typeOrdinal) ? reader.GetString(typeOrdinal) : string.Empty,
+                                RxNumber = !reader.IsDBNull(rxNumberOrdinal) ? reader.GetString(rxNumberOrdinal) : string.Empty,
+                                RxDate = reader.GetDateTime(rxDateOrdinal),
+                                InsuranceCarrier = !reader.IsDBNull(insuranceCarrierOrdinal) ? reader.GetString(insuranceCarrierOrdinal) : string.Empty,
+                                DiaryNote = !reader.IsDBNull(diaryNoteOrdinal) ? reader.GetString(diaryNoteOrdinal) : string.Empty
+                            };
+                            retValResults.Add(record);
+                        }
+                    });
+                    retVal.DiaryResults = retValResults;
+                    retVal.TotalRowCount = totalRowsParam.Value as int? ?? default(int);
+                    return retVal;
+                });
+            });
+            /*var startDateParam = new SqlParameter
             {
                 ParameterName = "StartDate",
                 DbType = DbType.Date,
@@ -73,7 +179,7 @@ namespace BridgeportClaims.Data.DataProviders.Diaries
                 "@EndDate = :EndDate, @SortColumn = :SortColumn, @SortDirection = :SortDirection, @PageNumber = :PageNumber, @PageSize = :PageSize, @Closed = :Closed",
                 new List<SqlParameter> { isDefaultSortParam, startDateParam, endDateParam, sortColumnParam, sortDirectionParam, pageNumberParam, pageSizeParam, closedParam })?.ToList();
             return retVal;
-        }
+        }*/
 
         public void RemoveDiary(int prescriptionNoteId)
         {
--- Server/DAL/BridgeportClaims.Data/Dtos/DiariesDto.cs
+++ Server/DAL/BridgeportClaims.Data/Dtos/DiariesDto.cs
@@ -1,22 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BridgeportClaims.Data.Dtos
 {
     [Serializable]
     public sealed class DiariesDto
     {
-        public int DiaryId { get; set; }
-        public int ClaimId { get; set; }
-        public int PrescriptionNoteId { get; set; }
-        public string Owner { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime FollowUpDate { get; set; }
-        public string PatientName { get; set; }
-        public string ClaimNumber { get; set; }
-        public string Type { get; set; }
-        public string RxNumber { get; set; }
-        public DateTime RxDate { get; set; }
-        public string InsuranceCarrier { get; set; }
-        public string DiaryNote { get; set; }
+        public int TotalRowCount { get; set; }
+        public IList<DiaryResultDto> DiaryResults { get; set; }
     }
 }
