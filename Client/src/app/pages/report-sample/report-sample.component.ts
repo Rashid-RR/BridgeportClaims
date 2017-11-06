45997cb5d263417ed9c6bc3f65d04e28e53d1be8
Josephat <jogwayi@oosint.com>
2017-11-06 15:45:25 +0300

    references #452

--- Client/src/app/pages/report-sample/report-sample.component.ts
+++ Client/src/app/pages/report-sample/report-sample.component.ts
@@ -46,7 +46,7 @@ export class ReportSampleComponent implements OnInit ,AfterViewInit{
           })
           var chart = Highcharts.chart('container', {      
               title: {
-                  text: 'Revenue From Last 21 Days'
+                  text: 'Revenue From Last '+this.data.length+' Days'
               },
               chart: {
                 type: 'column'
