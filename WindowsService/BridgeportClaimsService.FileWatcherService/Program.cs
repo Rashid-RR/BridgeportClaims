f1370fe4b83be2a12ec95ec1debd540eaec2eb53
Jordan Gurney <jordangurney@gmail.com>
2017-11-26 19:11:51 -0700

    Changes

--- WindowsService/BridgeportClaimsService.FileWatcherService/Program.cs
+++ WindowsService/BridgeportClaimsService.FileWatcherService/Program.cs
@@ -10,6 +10,8 @@ namespace BridgeportClaimsService.FileWatcherService
         /// </summary>
         internal static void Main()
         {
+            var service = new BridgeportClaimsWindowsService();
+            service.OnDebug();
             var servicesToRun = new ServiceBase[]
             {
                 new BridgeportClaimsWindowsService()
