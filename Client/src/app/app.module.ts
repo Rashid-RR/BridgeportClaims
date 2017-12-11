685c5d2c60d7dc9724fc7c58c1493797f316afea
Jordan Gurney <jordangurney@gmail.com>
2017-12-11 04:31:08 -0700

    Fixing

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -12,8 +12,8 @@ import { FileUploadModule } from 'ng2-file-upload';
 import {Ng2Webstorage} from 'ng2-webstorage';
 import { AutoCompleteModule} from './auto-complete';
 /* import { SignalRModule ,SignalRConfiguration } from 'ng2-signalr'; */
-import {WindowsInjetor,WindowBackdrop,BootstrapWindowContainer} from "./components/ng-window";
-// Layouts 
+import {WindowsInjetor, WindowBackdrop, BootstrapWindowContainer} from './components/ng-window';
+// Layouts
 import {HeaderComponent} from './layouts/header/header.component';
 import {AppLayoutComponent} from './layouts/app-layout.component';
 import {SidebarComponent} from './layouts/sidebar/sidebar.component';
@@ -33,7 +33,7 @@ import { DiaryResultsComponent } from './components/diary-results/diary-results.
 import { DiaryComponent } from './pages/diary/diary.component';
 import { UnpaidScriptComponent } from './pages/unpaid-script/unpaid-script.component';
  // services
-import { SignalRService, DiaryService, HttpService, AuthGuard, ProfileManager, EventsService, ClaimManager, 
+import { SignalRService, DiaryService, HttpService, AuthGuard, ProfileManager, EventsService, ClaimManager,
   PaymentScriptService,UnpaidScriptService,AccountReceivableService,ReportLoaderService,DocumentManagerService} from './services/services.barrel';
 import { PayorsComponent } from './pages/payors/payors.component';
 import { ClaimsComponent } from './pages/claim/claim.component';
@@ -72,7 +72,7 @@ import { UnindexedImageFileComponent } from './pages/unindexed-image-file/uninde
 import { IndexFileComponent } from './pages/index-file/index-file.component';
 import { UnindexedImageFileListComponent } from './pages/unindexed-image-file-list/unindexed-image-file-list.component';
 import { SignalrDocmentDemoComponent } from './pages/signalr-docment-demo/signalr-docment-demo.component';
- 
+
 @Pipe({name: 'safeStyle'})
 export class SafeStylePipe implements PipeTransform {
   constructor(private sanitized: DomSanitizer) {}
@@ -129,7 +129,7 @@ export class SafeUrlPipe implements PipeTransform {
   providers: [
     DecimalPipe, DatePipe,DiariesFilterPipe, HttpService, ProfileManager, EventsService, AuthGuard, ClaimManager, PaymentService,
     PaymentScriptService, DiaryService,WindowsInjetor,UnpaidScriptService,AccountReceivableService,ReportLoaderService,SignalRService,
-    DocumentManagerService, 
+    DocumentManagerService,
     {
       provide: LocationStrategy,
       useClass: HashLocationStrategy
@@ -140,5 +140,5 @@ export class SafeUrlPipe implements PipeTransform {
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  
+
 }
