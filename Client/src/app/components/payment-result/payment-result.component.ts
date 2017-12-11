cff250fa6dcc572dddb0a602e361e032b2e8a76b
Josephat <jogwayi@oosint.com>
2017-12-11 14:42:10 +0300

    variable scope bug fixes

--- Client/src/app/components/payment-result/payment-result.component.ts
+++ Client/src/app/components/payment-result/payment-result.component.ts
@@ -1,11 +1,4 @@
 import { Component,Optional, OnInit } from '@angular/core';
-import {HttpService} from "../../services/http-service";
-import {PaymentService} from "../../services/payment-service";
-import {EventsService} from "../../services/events-service";
-import {PaymentClaim} from "../../models/payment-claim";
-import {ToastsManager } from 'ng2-toastr/ng2-toastr';
-import {Router} from "@angular/router";
-
 declare var jQuery:any;
 
 @Component({
@@ -15,9 +8,9 @@ declare var jQuery:any;
 })
 export class PaymentResultComponent implements OnInit {
 
-  constructor(@Optional() public paymentService:PaymentService, private http: HttpService, private router: Router, private events: EventsService,private toast: ToastsManager) {
+  constructor() {
 
-   }
+  }
 
   ngOnInit() {
     
