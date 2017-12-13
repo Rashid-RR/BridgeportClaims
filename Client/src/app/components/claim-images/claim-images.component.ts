04a96784478d9710bd640ebf509d29ecc0159ac4
Josephat <jogwayi@oosint.com>
2017-12-13 13:22:25 +0300

    references #505

--- Client/src/app/components/claim-images/claim-images.component.ts
+++ Client/src/app/components/claim-images/claim-images.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ClaimManager} from "../../services/claim-manager";
 import {ClaimImage} from "../../models/claim-image";
+import { SortColumnInfo } from '../../directives/table-sort.directive';
+import { HttpService } from '../../services/http-service';
 
 @Component({
   selector: 'app-claim-images',
@@ -9,7 +11,11 @@ import {ClaimImage} from "../../models/claim-image";
 })
 export class ClaimImagesComponent implements OnInit {
 
-  constructor(public claimManager:ClaimManager) { }
+
+  sortColumn: SortColumnInfo;
+  constructor(
+    public claimManager:ClaimManager,
+    private http: HttpService) { }
 
   ngOnInit() {
 
@@ -17,6 +23,37 @@ export class ClaimImagesComponent implements OnInit {
   openAttachment(image:ClaimImage){
     console.log("Open attachmen")
     window.open('//pathToPDF',"_tab"); 
+  }
+  onSortColumn(info: SortColumnInfo) {
+    this.sortColumn = info;
+    this.fetchData();
+  }
+
+  fetchData() {
+  this.claimManager.loadingImage = true;
+    var data = {
+      claimId:this.claimManager.selectedClaim.claimId,
+      date: null,
+      isIndexed:true,
+      sort: "DocumentID",
+      sortDirection: "ASC",
+      page: 1,
+      pageSize: 500
+    };
+    let sort = 'RxDate';
+    let sort_dir: 'asc' | 'desc' = 'desc';
+    if (this.sortColumn) {
+      data.sort = this.sortColumn.column;
+      data.sortDirection = this.sortColumn.dir;
+    }
+    
+    this.http.getDocuments(data).map(p => p.json())
+      .subscribe(results => {
+        this.claimManager.selectedClaim.setImages(results.documentResults);
+        this.claimManager.loadingImage = false;
+      },err=>{
+        this.claimManager.loadingImage = false;
+      });
   }
 
 }
--- Client/src/app/models/claim.ts
+++ Client/src/app/models/claim.ts
@@ -1,131 +1,142 @@
 // profile.ts
-import {Prescription} from "./prescription";
-import {Payment} from "./payment";
-import {ClaimImage} from "./claim-image";
-import {PrescriptionNote} from "./prescription-note";
-import {ClaimNote} from "./claim-note";
-import {Episode} from "./episode";
-import {ClaimFlex2} from "./claim-flex2";
-import {PrescriptionStatuses} from "./prescription-statuses";
+import { Prescription } from "./prescription";
+import { Payment } from "./payment";
+import { DocumentItem } from "./document";
+import { DocumentType } from "./document-type";
+import { ClaimImage } from "./claim-image";
+import { PrescriptionNote } from "./prescription-note";
+import { ClaimNote } from "./claim-note";
+import { Episode } from "./episode";
+import { ClaimFlex2 } from "./claim-flex2";
+import { PrescriptionStatuses } from "./prescription-statuses";
 import * as Immutable from 'immutable';
 
- 
-export class Claim {
-    claimId:Number;
-    name:String;
-    firstName:String;
-    lastName:String;
-    claimNumber:Number;
-    dateOfBirth:Date ;
-    injuryDate:Date ;
-    gender:String;
-    carrier:String;
-    adjustor:String;
-    adjustorPhoneNumber:String;
-    dateEntered:Date;
-    eligibilityTermDate:Date;
-    adjustorFaxNumber:String;
-    flex2:String;
-    private prescription:Array<Prescription> = [];
-    //private prescriptionNote:Array<PrescriptionNote> = [];
-    private prescriptionNote:Immutable.OrderedMap<Number, PrescriptionNote> = Immutable.OrderedMap<Number, PrescriptionNote>();
-    private payment:Array<Payment> = [];
-    private image:Array<ClaimImage> = [];
-    private episode:Array<Episode> = [];
-    private claimFlex2s:Array<ClaimFlex2> = [];
-    private prescriptionStatus:Array<PrescriptionStatuses> = [];
-    claimNote:ClaimNote;
-    editing:Boolean=false;
-constructor(claimId:Number,claimNumber:Number,dateOfBirth:Date,injuryDate:Date,
-    gender:String,carrier:String,adjustor:String,adjustorPhoneNumber:String,dateEntered:Date,adjustorFaxNumber:String,name?:String,firstName?:String,lastName?:String,flex2?:String,eligibilityTermDate?:Date){
-    this.claimId = claimId;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.name = name;
-    this.claimNumber = claimNumber;
-    this.dateOfBirth = dateOfBirth ;
-    this.injuryDate = injuryDate ;
-    this.gender = gender;
-    this.flex2 = flex2;
-    this.carrier = carrier;
-    this.adjustor= adjustor;
-    this.adjustorPhoneNumber = adjustorPhoneNumber;
-    this.dateEntered = dateEntered;
-    this.adjustorFaxNumber = adjustorFaxNumber;
-    this.eligibilityTermDate = eligibilityTermDate;
-  }
 
-  setPrescription(prescription:Array<Prescription>){
-      if(prescription){
-        this.prescription = prescription
-      }
-  }
-  get prescriptions():Array<Prescription>{
-      return this.prescription
-  }
-  setPayment(payments:Array<Payment>){
-      if(payments){
-        this.payment = payments
-      }
-  }
-  get payments():Array<Payment>{
-      return this.payment
-  }
-  setImages(images:Array<ClaimImage>){
-    if(images){
-      this.image = images
+export class Claim {
+    claimId: Number;
+    name: String;
+    firstName: String;
+    lastName: String;
+    claimNumber: Number;
+    dateOfBirth: Date;
+    injuryDate: Date;
+    gender: String;
+    carrier: String;
+    adjustor: String;
+    adjustorPhoneNumber: String;
+    dateEntered: Date;
+    eligibilityTermDate: Date;
+    adjustorFaxNumber: String;
+    flex2: String;
+    private prescription: Array<Prescription> = [];
+    //private prescriptionNote:Array<PrescriptionNote> = [];
+    private prescriptionNote: Immutable.OrderedMap<Number, PrescriptionNote> = Immutable.OrderedMap<Number, PrescriptionNote>();
+    private payment: Array<Payment> = [];
+    private image: Array<DocumentItem> = [];
+    private documentType: Array<DocumentType> = [];
+    private episode: Array<Episode> = [];
+    private claimFlex2s: Array<ClaimFlex2> = [];
+    private prescriptionStatus: Array<PrescriptionStatuses> = [];
+    claimNote: ClaimNote;
+    editing: Boolean = false;
+    constructor(claimId: Number, claimNumber: Number, dateOfBirth: Date, injuryDate: Date,
+        gender: String, carrier: String, adjustor: String, adjustorPhoneNumber: String, dateEntered: Date, adjustorFaxNumber: String, name?: String, firstName?: String, lastName?: String, flex2?: String, eligibilityTermDate?: Date) {
+        this.claimId = claimId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.name = name;
+        this.claimNumber = claimNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.injuryDate = injuryDate;
+        this.gender = gender;
+        this.flex2 = flex2;
+        this.carrier = carrier;
+        this.adjustor = adjustor;
+        this.adjustorPhoneNumber = adjustorPhoneNumber;
+        this.dateEntered = dateEntered;
+        this.adjustorFaxNumber = adjustorFaxNumber;
+        this.eligibilityTermDate = eligibilityTermDate;
     }
+
+    setPrescription(prescription: Array<Prescription>) {
+        if (prescription) {
+            this.prescription = prescription
+        }
     }
-    get images():Array<ClaimImage>{
+    get prescriptions(): Array<Prescription> {
+        return this.prescription
+    }
+    setPayment(payments: Array<Payment>) {
+        if (payments) {
+            this.payment = payments
+        }
+    }
+    get payments(): Array<Payment> {
+        return this.payment
+    }
+    setImages(images: Array<DocumentItem>) {
+        if (images) {
+            this.image = images
+        }
+    }
+    get images(): Array<DocumentItem> {
         return this.image
     }
-    setEpisodes(episodes:Array<Episode>){
-        if(episodes){
+    setDocumentTypes(types: Array<DocumentType>) {
+        if (types) {
+            this.documentType = types
+        }
+    }
+    get documentTypes(): Array<DocumentType> {
+        return this.documentType
+    }
+    setEpisodes(episodes: Array<Episode>) {
+        if (episodes) {
             this.episode = episodes;
         }
     }
-    get episodes():Array<Episode>{
+    get episodes(): Array<Episode> {
         return this.episode;
     }
-    setFlex2(claimFlex2s:Array<ClaimFlex2>){
-        if(claimFlex2s){
+    setFlex2(claimFlex2s: Array<ClaimFlex2>) {
+        if (claimFlex2s) {
             this.claimFlex2s = claimFlex2s;
         }
     }
-    get getFlex2():Array<ClaimFlex2>{
+    get getFlex2(): Array<ClaimFlex2> {
         return this.claimFlex2s;
     }
-    setPrescriptionNotes(prescriptionNotes:Array<PrescriptionNote>){
-        if(prescriptionNotes){
-            prescriptionNotes.forEach(note=>{ 
+    setPrescriptionNotes(prescriptionNotes: Array<PrescriptionNote>) {
+        if (prescriptionNotes) {
+            prescriptionNotes.forEach(note => {
                 let n = this.prescriptionNote.get(note.prescriptionNoteId);
-                if(note.scripts && note.scripts.length>0){
+                if (note.scripts && note.scripts.length > 0) {
                     note.rxDate = note.scripts[0].rxDate;
                     note.rxNumber = note.scripts[0].rxNumber;
                 }
-                if(!n || n.rxDate.getTime()<note.rxDate.getTime()){
-                    this.prescriptionNote = this.prescriptionNote.set(note.prescriptionNoteId, note); 
-                }             
+                if (!n || n.rxDate.getTime() < note.rxDate.getTime()) {
+                    this.prescriptionNote = this.prescriptionNote.set(note.prescriptionNoteId, note);
+                }
             })
         }
     }
-    get prescriptionStatuses():Array<PrescriptionStatuses>{
+    get prescriptionStatuses(): Array<PrescriptionStatuses> {
         return this.prescriptionStatus;
     }
-    setPrescriptionStatuses(prescriptionStatuses:Array<PrescriptionStatuses>){
-        if(prescriptionStatuses){
+    setPrescriptionStatuses(prescriptionStatuses: Array<PrescriptionStatuses>) {
+        if (prescriptionStatuses) {
             this.prescriptionStatus = prescriptionStatuses
         }
     }
-    get prescriptionNotes():Array<PrescriptionNote>{
+    get prescriptionNotes(): Array<PrescriptionNote> {
         return this.prescriptionNote.toArray();
     }
-    setClaimNotes(claimNote:ClaimNote){
-        if(claimNote){
+    setClaimNotes(claimNote: ClaimNote) {
+        if (claimNote) {
             this.claimNote = claimNote;
         }
     }
-    get claimNotes():ClaimNote{
+    get claimNotes(): ClaimNote {
         return this.claimNote;
     }
 }
