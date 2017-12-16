9a736d147dcfae19f2e0f97f2cd3762560001926
Jordan Gurney <jordangurney@gmail.com>
2017-12-15 19:09:21 -0700

    Claim Image

--- Server/BridgeportClaims.Web/Models/EditClaimImageViewModel.cs
+++ Server/BridgeportClaims.Web/Models/EditClaimImageViewModel.cs
@@ -1,26 +1,15 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using BridgeportClaims.Common.Extensions;
 
 namespace BridgeportClaims.Web.Models
 {
     [Serializable]
     public sealed class EditClaimImageViewModel
     {
-        private string _type;
-
         [Required]
         public int DocumentId { get; set; }
-
         [Required]
-        public string Type
-        {
-            get => _type.IsNotNullOrWhiteSpace() ? _type.ToLower() : null;
-            set => _type = value;
-        }
-
-        public string TypeNormal => _type;
-
+        public byte DocumentTypeId { get; set; }
         public DateTime? RxDate { get; set; }
         public string RxNumber { get; set; }
     }
