d00a4db53ea13a89c64a1c64b3b6341737d901d9
Jordan Gurney <jordangurney@gmail.com>
2017-12-18 01:10:49 -0700

    Changes.

---
+++ Database/Schema/Stored Procedures/dbo.uspGetClaimUserHistory.sql
@@ -0,0 +1,27 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+/*
+	Author:			Jordan Gurney
+	Date:			12/18/2017
+	Description:	Proc to replace NHibernate query to get the user history results
+					for recently viewed Claims.
+	Example Execute:
+					DECLARE @UserID NVARCHAR(128)
+					SELECT TOP 1 @UserID = ID FROM dbo.AspNetUsers ORDER BY NEWID()
+					EXEC dbo.uspGetClaimUserHistory @UserID
+*/
+CREATE PROCEDURE [dbo].[uspGetClaimUserHistory] @UserID NVARCHAR(128)
+AS BEGIN
+	SELECT          ClaimId = v.[ClaimID]
+				  , v.[ClaimNumber]
+				  , v.[Name]
+				  , v.[InjuryDate]
+				  , v.[Carrier]
+				  , CreatedOnUtc = c.[CreatedOnUTC]
+	FROM            dbo.[vwClaimInfo]         AS v WITH (NOEXPAND)
+		INNER JOIN  [dbo].[ClaimsUserHistory] AS c ON [c].[ClaimID] = [v].[ClaimID]
+	WHERE	[c].[UserID] = @UserID
+END
+GO
--- Database/Schema/Views/dbo.vwClaimInfo.sql
+++ Database/Schema/Views/dbo.vwClaimInfo.sql
@@ -2,16 +2,21 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 CREATE VIEW [dbo].[vwClaimInfo]
+WITH SCHEMABINDING
 AS
-SELECT ROW_NUMBER() OVER (ORDER BY [c].[ClaimNumber]) RowID
-	 , [c].[ClaimID]
+SELECT [c].[ClaimID]
 	 , [c].[ClaimNumber]
      , [Name] = NULLIF(ISNULL(LTRIM(RTRIM([p].[FirstName])), '') + ' ' + ISNULL(LTRIM(RTRIM([p].[LastName])), ''), ' ')
      , Carrier = [pa].[BillToName]
-     , InjuryDate = FORMAT([c].[DateOfInjury], 'M/d/yyyy')
+     , InjuryDate = [c].[DateOfInjury]
 FROM   [dbo].[Claim] AS [c]
-       LEFT JOIN [dbo].[Patient] AS [p] ON [p].[PatientID] = [c].[PatientID]
-       LEFT JOIN [dbo].[Payor] AS pa ON [pa].[PayorID] = [c].[PayorID]
+       JOIN [dbo].[Patient] AS [p] ON [p].[PatientID] = [c].[PatientID]
+       JOIN [dbo].[Payor] AS pa ON [pa].[PayorID] = [c].[PayorID]
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [pkVwClaimInfoClaimID] ON [dbo].[vwClaimInfo] ([ClaimID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [idxUqVwClaimInfoClaimIDIncludeAll] ON [dbo].[vwClaimInfo] ([ClaimID]) INCLUDE ([Carrier], [ClaimNumber], [InjuryDate], [Name]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
 GO
