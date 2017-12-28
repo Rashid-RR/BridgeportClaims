f85214dfd934afbb6151830cdddc0310ae96975e
Jordan Gurney <jordangurney@gmail.com>
2017-12-28 16:14:01 -0700

    Prescriber table import.

---
+++ Database/Schema/Tables/dbo.Prescriber.sql
@@ -0,0 +1,22 @@
+CREATE TABLE [dbo].[Prescriber]
+(
+[PrescriberNPI] [varchar] (12) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ExpDate] [date] NULL,
+[PrescriberName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Addr1] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Addr2] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[City] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[StateID] [int] NULL,
+[Zip] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FLDOHNUM] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Phone] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Fax] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfPrescriberCreatedOnUTC] DEFAULT (sysutcdatetime()),
+[UpdatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfPrescriberUpdatedOnUTC] DEFAULT (sysutcdatetime()),
+[DataVersion] [timestamp] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Prescriber] ADD CONSTRAINT [pkPrescriber] PRIMARY KEY CLUSTERED  ([PrescriberNPI]) WITH (FILLFACTOR=90) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Prescriber] ADD CONSTRAINT [fkPrescriberStateIDUsStateStateID] FOREIGN KEY ([StateID]) REFERENCES [dbo].[UsState] ([StateID])
+GO
