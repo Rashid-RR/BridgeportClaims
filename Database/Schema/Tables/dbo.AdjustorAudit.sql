be0051ea32cf7dadb0bab40310679df2c86f9448
Jordan Gurney <jordangurney@gmail.com>
2018-01-20 03:18:16 -0700

    New audit triggers

---
+++ Database/Schema/Tables/dbo.AdjustorAudit.sql
@@ -0,0 +1,26 @@
+CREATE TABLE [dbo].[AdjustorAudit]
+(
+[AdjustorAuditID] [int] NOT NULL IDENTITY(1, 1),
+[AdjustorID] [int] NOT NULL,
+[PayorID] [int] NOT NULL,
+[AdjustorName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[PhoneNumber] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FaxNumber] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[EmailAddress] [varchar] (155) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Extension] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ModifiedByUserID] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreatedOnUTC] [datetime2] NOT NULL,
+[UpdatedOnUTC] [datetime2] NOT NULL,
+[DataVersion] [timestamp] NOT NULL,
+[ETLRowID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Operation] [varchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SystemUser] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AuditDateUTC] [datetime2] NOT NULL
+) ON [PRIMARY]
+WITH
+(
+DATA_COMPRESSION = ROW
+)
+GO
+ALTER TABLE [dbo].[AdjustorAudit] ADD CONSTRAINT [pkAdjustorAudit] PRIMARY KEY CLUSTERED  ([AdjustorAuditID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
+GO
--- Database/Schema/Tables/dbo.Claim.sql
+++ Database/Schema/Tables/dbo.Claim.sql
@@ -26,6 +26,52 @@ WITH
 DATA_COMPRESSION = ROW
 )
 GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE TRIGGER [dbo].[utClaimAudit] ON [dbo].[Claim] FOR INSERT, UPDATE, DELETE
+AS BEGIN
+	SET NOCOUNT ON;
+    IF ( SELECT COUNT(*)
+         FROM   INSERTED
+       ) > 0 
+        BEGIN 
+            IF ( SELECT COUNT(*)
+                 FROM   DELETED
+               ) > 0 
+                BEGIN 
+        
+                    INSERT  INTO dbo.ClaimAudit
+                            ( ClaimID, PolicyNumber, DateOfInjury, IsFirstParty, ClaimNumber, PreviousClaimNumber, PersonCode, PayorID, AdjusterID, JurisdictionStateID, RelationCode, TermDate, PatientID, ETLRowID, UniqueClaimNumber, ClaimFlex2ID, ModifiedByUserID, CreatedOnUTC, UpdatedOnUTC, Operation, SystemUser, AuditDateUTC)
+                            SELECT  ClaimID, PolicyNumber, DateOfInjury, IsFirstParty, ClaimNumber, PreviousClaimNumber, PersonCode, PayorID, AdjusterID, JurisdictionStateID, RelationCode, TermDate, PatientID, ETLRowID, UniqueClaimNumber, ClaimFlex2ID, ModifiedByUserID, CreatedOnUTC, UpdatedOnUTC,'UPDATE'
+                                   ,SUSER_SNAME()
+                                   ,SYSUTCDATETIME()
+                            FROM    INSERTED
+           
+                END 
+            ELSE 
+                BEGIN 
+                    INSERT  INTO dbo.ClaimAudit
+                            ( ClaimID, PolicyNumber, DateOfInjury, IsFirstParty, ClaimNumber, PreviousClaimNumber, PersonCode, PayorID, AdjusterID, JurisdictionStateID, RelationCode, TermDate, PatientID, ETLRowID, UniqueClaimNumber, ClaimFlex2ID, ModifiedByUserID, CreatedOnUTC, UpdatedOnUTC, Operation, SystemUser, AuditDateUTC
+                            )
+                            SELECT  ClaimID, PolicyNumber, DateOfInjury, IsFirstParty, ClaimNumber, PreviousClaimNumber, PersonCode, PayorID, AdjusterID, JurisdictionStateID, RelationCode, TermDate, PatientID, ETLRowID, UniqueClaimNumber, ClaimFlex2ID, ModifiedByUserID, CreatedOnUTC, UpdatedOnUTC,'INSERT'
+                                   ,SUSER_SNAME()
+                                   ,SYSUTCDATETIME()
+                            FROM    INSERTED
+                END 
+        END 
+    ELSE 
+        BEGIN 
+            INSERT  INTO dbo.ClaimAudit
+                    ( ClaimID, PolicyNumber, DateOfInjury, IsFirstParty, ClaimNumber, PreviousClaimNumber, PersonCode, PayorID, AdjusterID, JurisdictionStateID, RelationCode, TermDate, PatientID, ETLRowID, UniqueClaimNumber, ClaimFlex2ID, ModifiedByUserID, CreatedOnUTC, UpdatedOnUTC, Operation, SystemUser, AuditDateUTC)
+                    SELECT  ClaimID, PolicyNumber, DateOfInjury, IsFirstParty, ClaimNumber, PreviousClaimNumber, PersonCode, PayorID, AdjusterID, JurisdictionStateID, RelationCode, TermDate, PatientID, ETLRowID, UniqueClaimNumber, ClaimFlex2ID, ModifiedByUserID, CreatedOnUTC, UpdatedOnUTC,'DELETE'
+                           ,SUSER_SNAME()
+                           ,SYSUTCDATETIME()
+                    FROM    DELETED
+        END
+END
+GO
 ALTER TABLE [dbo].[Claim] ADD CONSTRAINT [ckClaimRelationCode] CHECK (([RelationCode]>=(1) AND [RelationCode]<=(9)))
 GO
 ALTER TABLE [dbo].[Claim] ADD CONSTRAINT [pkClaim] PRIMARY KEY CLUSTERED  ([ClaimID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
