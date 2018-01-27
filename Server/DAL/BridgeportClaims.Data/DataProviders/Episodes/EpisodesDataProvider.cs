d7a3720703338a94bf9c0924db0689e1a81ac62a
Jordan Gurney <jordangurney@gmail.com>
2018-01-27 06:37:17 -0700

    Added a killer Episode sorting API call

--- Server/DAL/BridgeportClaims.Data/DataProviders/Episodes/EpisodesDataProvider.cs
+++ Server/DAL/BridgeportClaims.Data/DataProviders/Episodes/EpisodesDataProvider.cs
@@ -9,7 +9,6 @@ using BridgeportClaims.Data.Dtos;
 using BridgeportClaims.Data.Repositories;
 using BridgeportClaims.Entities.DomainModels;
 using cs = BridgeportClaims.Common.Config.ConfigService;
-using c = BridgeportClaims.Common.StringConstants.Constants;
 
 namespace BridgeportClaims.Data.DataProviders.Episodes
 {
@@ -17,13 +16,90 @@ namespace BridgeportClaims.Data.DataProviders.Episodes
 	public class EpisodesDataProvider : IEpisodesDataProvider
 	{
 		private readonly IRepository<EpisodeType> _episodeTypeRepository;
-	    private readonly IMemoryCacher _memoryCacher;
 
 		public EpisodesDataProvider(IRepository<EpisodeType> episodeTypeRepository)
 		{
 		    _episodeTypeRepository = episodeTypeRepository;
-		    _memoryCacher = MemoryCacher.Instance;
 		}
+
+	    public IList<EpisodeResultsDto> GetEpisodes(bool resolved, string sortColumn, string sortDirection, int pageNumber) =>
+	        DisposableService.Using(() => new SqlConnection(cs.GetDbConnStr()), conn =>
+	        {
+	            return DisposableService.Using(() => new SqlCommand("[dbo].[uspGetEpisodes]", conn), cmd =>
+	            {
+	                cmd.CommandType = CommandType.StoredProcedure;
+	                var resolvedParam = cmd.CreateParameter();
+	                resolvedParam.Direction = ParameterDirection.Input;
+	                resolvedParam.Value = resolved;
+	                resolvedParam.ParameterName = "@Resolved";
+	                resolvedParam.DbType = DbType.Boolean;
+	                resolvedParam.SqlDbType = SqlDbType.Bit;
+	                cmd.Parameters.Add(resolvedParam);
+	                var sortColumnParam = cmd.CreateParameter();
+	                sortColumnParam.Value = sortColumn ?? (object) DBNull.Value;
+                    sortColumnParam.Direction = ParameterDirection.Input;
+                    sortColumnParam.DbType= DbType.AnsiString;
+	                sortColumnParam.Size = 50;
+	                sortColumnParam.ParameterName = "@SortColumn";
+	                sortColumnParam.SqlDbType = SqlDbType.VarChar;
+                    cmd.Parameters.Add(sortColumnParam);
+	                var sortDirectionParam = cmd.CreateParameter();
+	                sortDirectionParam.Value = sortDirection ?? (object) DBNull.Value;
+	                sortDirectionParam.ParameterName = "@SortDirection";
+                    sortDirectionParam.DbType = DbType.AnsiString;
+	                sortDirectionParam.Size = 5;
+                    sortDirectionParam.Direction = ParameterDirection.Input;
+	                sortDirectionParam.SqlDbType = SqlDbType.VarChar;
+                    cmd.Parameters.Add(sortDirectionParam);
+	                var pageNumberParam = cmd.CreateParameter();
+	                pageNumberParam.Value = default(int) == pageNumber ? 5000 : pageNumber;
+	                pageNumberParam.DbType = DbType.Int32;
+                    pageNumberParam.SqlDbType = SqlDbType.Int;
+                    pageNumberParam.Direction = ParameterDirection.Input;
+	                pageNumberParam.ParameterName = "@PageNumber";
+                    cmd.Parameters.Add(pageNumberParam);
+	                var totalPageSizeParam = cmd.CreateParameter();
+	                totalPageSizeParam.ParameterName = "@TotalPageSize";
+                    totalPageSizeParam.Direction = ParameterDirection.Output;
+                    totalPageSizeParam.DbType = DbType.Int32;
+                    totalPageSizeParam.SqlDbType = SqlDbType.Int;
+                    cmd.Parameters.Add(totalPageSizeParam);
+                    var retVal = new List<EpisodeResultsDto>();
+	                if (conn.State != ConnectionState.Open)
+	                    conn.Open();
+                    DisposableService.Using(cmd.ExecuteReader, reader =>
+                    {
+                        var episodeIdOrdinal = reader.GetOrdinal("EpisodeId");
+                        var ownerOrdinal = reader.GetOrdinal("Owner");
+                        var createdOrdinal = reader.GetOrdinal("Created");
+                        var patientNameOrdinal = reader.GetOrdinal("Patient");
+                        var claimNumberOrdinal = reader.GetOrdinal("ClaimNumber");
+                        var typeOrdinal = reader.GetOrdinal("Type");
+                        var pharmacyOrdinal = reader.GetOrdinal("Pharmacy");
+                        var carrierOrdinal = reader.GetOrdinal("Carrier");
+                        var episodeNoteOrdinal = reader.GetOrdinal("EpisodeNote");
+                        while (reader.Read())
+                        {
+                            var result = new EpisodeResultsDto
+                            {
+                                EpisodeId = !reader.IsDBNull(episodeIdOrdinal) ? reader.GetInt32(episodeIdOrdinal) : throw new Exception("Error, there cannot be a null Episode ID"),
+                                Owner = !reader.IsDBNull(ownerOrdinal) ? reader.GetString(ownerOrdinal) : string.Empty,
+                                Created = !reader.IsDBNull(createdOrdinal) ? reader.GetDateTime(createdOrdinal) : (DateTime?) null,
+                                PatientName = !reader.IsDBNull(patientNameOrdinal) ? reader.GetString(patientNameOrdinal) : string.Empty,
+                                ClaimNumber = !reader.IsDBNull(claimNumberOrdinal) ? reader.GetString(claimNumberOrdinal) : string.Empty,
+                                Type = !reader.IsDBNull(typeOrdinal) ? reader.GetString(typeOrdinal) : string.Empty,
+                                Pharmacy = !reader.IsDBNull(pharmacyOrdinal) ? reader.GetString(pharmacyOrdinal) : string.Empty,
+                                Carrier = !reader.IsDBNull(carrierOrdinal) ? reader.GetString(carrierOrdinal) : string.Empty,
+                                EpisodeNote = !reader.IsDBNull(episodeNoteOrdinal) ? reader.GetString(episodeNoteOrdinal) : string.Empty
+                            };
+                            retVal.Add(result);
+                        }
+                    });
+	                if (conn.State != ConnectionState.Closed)
+	                    conn.Close();
+	                return retVal;
+	            });
+	        });
 
 		public void AddOrUpdateEpisode(int? episodeId, int claimId, string by, string noteText, int? episodeTypeId)
 		{
