083b5fd7ee817c810976c969109c81cbbf3838f4
Josephat <jogwayi@oosint.com>
2018-02-05 00:25:02 +0300

    references #671

--- Client/src/app/services/report-loader.service.ts
+++ Client/src/app/services/report-loader.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router,NavigationEnd } from '@angular/router';
 
 @Injectable()
 export class ReportLoaderService {
   loading: Boolean = false; 
   current:String='List';
   currentURL:String='List';
-  constructor() { }
+  routes:string[]=[];
+  constructor(private router:Router) { 
+      this.router.events.subscribe(e=>{
+        if(e instanceof NavigationEnd){
+          this.routes=this.router.url.split("/");
+          console.log(this.router.url,this.routes);
+        }
+      })
+  }
 
 }
