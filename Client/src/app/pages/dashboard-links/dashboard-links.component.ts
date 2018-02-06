b03d134f352bc00dd4b72fb3869ec60cc7a39515
Josephat <jogwayi@oosint.com>
2018-02-06 21:28:12 +0300

    references #679

--- Client/src/app/pages/dashboard-links/dashboard-links.component.ts
+++ Client/src/app/pages/dashboard-links/dashboard-links.component.ts
@@ -15,21 +15,21 @@ export class DashboardLinksComponent implements OnInit, AfterViewInit {
   categories: Array<any> = [];
   data: Array<any> = [];
   summary: any = {
-    lastWorkDate: null,
-    totalImagesScanned: null,
-    totalImagesIndexed: null,
-    totalImagesRemaining: null,
-    diariesAdded: null,
-    newClaims: null,
-    newEpisodes: null,
-    newInvoicesPrinted: null,
-    newPaymentsPosted: null,
-    newPrescriptions: null,
-    newReversedPrescriptions: null,
-    totalDiariesResolved: null,
-    totalDiariesUnResolved: null,
-    totalResolvedEpisodes: null,
-    totalUnresolvedEpisodes: null
+    lastWorkDate:  "2018-02-05T00:00:00.0000000",
+    totalImagesScanned: 70,
+    totalImagesIndexed: 33,
+    totalImagesRemaining: 113,
+    diariesAdded: 53,
+    newClaims: 53,
+    newEpisodes: 22,
+    newInvoicesPrinted: 6,
+    newPaymentsPosted: 33,
+    newPrescriptions: 90,
+    newReversedPrescriptions: 39,
+    totalDiariesResolved: 41,
+    totalDiariesUnResolved: 54,
+    totalResolvedEpisodes: 76,
+    totalUnresolvedEpisodes: 11
   }
   constructor(
     private http: HttpService,
@@ -45,13 +45,23 @@ export class DashboardLinksComponent implements OnInit, AfterViewInit {
     this.http.getKPIs().map(res => { return res.json(); })
       .subscribe((result: any) => {
         console.log(result);
-        this.summary = result;
+        //this.summary = result;
       }, err => null);
   }
 
   get allowed(): Boolean {
     return (this.profileManager.profile && this.profileManager.profile.roles && (this.profileManager.profile.roles instanceof Array)
       && this.profileManager.profile.roles.indexOf('Admin') > -1);
+  }
+  formatDate(input: String) {
+    if (!input) return null;
+    if (input.indexOf("-") > -1) {
+      let date = input.split("T");
+      let d = date[0].split("-");
+      return d[1] + "/" + d[2] + "/" + d[0];
+    } else {
+      return input;
+    }
   }
 
 }
