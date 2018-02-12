8f6e8cdd3eada0c4be5bb4605f23dc89b5b90031
Jordan Gurney <jordangurney@gmail.com>
2018-02-11 17:33:11 -0700

    Finished Firewall API's

--- Server/BridgeportClaims.Web/Controllers/AdminController.cs
+++ Server/BridgeportClaims.Web/Controllers/AdminController.cs
@@ -2,7 +2,9 @@
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
+using BridgeportClaims.Common.Extensions;
 using BridgeportClaims.Data.DataProviders.AdminFunctions;
+using BridgeportClaims.Web.Models;
 using NLog;
 
 namespace BridgeportClaims.Web.Controllers
@@ -59,14 +61,20 @@ namespace BridgeportClaims.Web.Controllers
 
         [HttpPost]
         [Route("create-firewall-setting")]
-        public async Task<IHttpActionResult> CreateFirewallSetting()
+        public async Task<IHttpActionResult> CreateFirewallSetting(CreateFirewallSettingModel model)
         {
             try
             {
                 return await Task.Run(() =>
                 {
-                    _adminFunctionsProvider.AddFirewallSetting("","","");
-                    return Ok(new { message = $"The firewall rule '' was created successfully." });
+                    if (model.RuleName.IsNullOrWhiteSpace())
+                        throw new ArgumentNullException(nameof(model.RuleName));
+                    if (model.StartIpAddress.IsNullOrWhiteSpace())
+                        throw new ArgumentNullException(nameof(model.StartIpAddress));
+                    if (model.EndIpAddress.IsNullOrWhiteSpace())
+                        throw new ArgumentNullException(nameof(model.EndIpAddress));
+                    _adminFunctionsProvider.AddFirewallSetting(model.RuleName, model.StartIpAddress, model.EndIpAddress);
+                    return Ok(new { message = $"The firewall rule '{model.RuleName}' was created successfully." });
                 });
             }
             catch (Exception ex)
