6d4b0a5fdee529e1fe1a3aea267fb99cc0d34ebb
Jordan Gurney <jgurney@cricut.com>
2018-02-16 13:09:25 -0700

    DB Changes

--- Database/Schema/Data/etl.LatestStagedLakerFileLoaded_Data.sql
+++ Database/Schema/Data/etl.LatestStagedLakerFileLoaded_Data.sql
@@ -1,3 +1,3 @@
 SET IDENTITY_INSERT [etl].[LatestStagedLakerFileLoaded] ON
-INSERT INTO [etl].[LatestStagedLakerFileLoaded] ([ID], [LastFileNameLoaded]) VALUES (1, 'Billing_Claim_File_20180113.csv')
+INSERT INTO [etl].[LatestStagedLakerFileLoaded] ([ID], [LastFileNameLoaded]) VALUES (1, 'Billing_Claim_File_20180210.csv')
 SET IDENTITY_INSERT [etl].[LatestStagedLakerFileLoaded] OFF
