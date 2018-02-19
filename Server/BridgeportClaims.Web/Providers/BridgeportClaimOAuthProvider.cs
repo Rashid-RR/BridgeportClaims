a170b49d37cb5ba723f225aa971dbf6a57406458
Jordan Gurney <jordangurney@gmail.com>
2018-02-19 12:34:09 -0700

    Versioning

--- Server/BridgeportClaims.Web/Providers/BridgeportClaimOAuthProvider.cs
+++ Server/BridgeportClaims.Web/Providers/BridgeportClaimOAuthProvider.cs
@@ -14,45 +14,45 @@ namespace BridgeportClaims.Web.Providers
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         private const string InvalidGrant = "invalid_grant";
 
-        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext ctx)
         {
-            context.Validated();
+            ctx.Validated();
             return Task.FromResult<object>(null);
         }
 
-        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext ctx)
         {
             try
             {
-                var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
-                var user = await userManager.FindAsync(context.UserName, context.Password);
+                var userManager = ctx.OwinContext.GetUserManager<ApplicationUserManager>();
+                var user = await userManager.FindAsync(ctx.UserName, ctx.Password);
                 if (null == user)
                 {
-                    context.SetError(InvalidGrant, "The user name or password is incorrect.");
+                    ctx.SetError(InvalidGrant, "The user name or password is incorrect.");
                     return;
                 }
                 if (!user.EmailConfirmed)
                 {
-                    context.SetError(InvalidGrant, "User did not confirm email.");
+                    ctx.SetError(InvalidGrant, "User did not confirm email.");
                     return;
                 }
                 // Special validation that the user is a part of a role in order to be granted authorization to login.
                 var hasAnyRoles = user.Roles?.Any();
                 if (null == hasAnyRoles || !hasAnyRoles.Value)
                 {
-                    context.SetError(InvalidGrant, $"User {user.UserName} is not yet a part of any roles. " +
+                    ctx.SetError(InvalidGrant, $"User {user.UserName} is not yet a part of any roles. " +
                                                       "Please make this user at least a member of the \"User\" role group.");
                     return;
                 }
                 if (user.LockoutEnabled && user.LockoutEndDateUtc.HasValue && DateTime.UtcNow < user.LockoutEndDateUtc)
                 {
-                    context.SetError(InvalidGrant, $"User {user.UserName} has been deactivated from the system. A user with the" +
+                    ctx.SetError(InvalidGrant, $"User {user.UserName} has been deactivated from the system. A user with the" +
                                                    " Admin role may activate them on the 'Manage Users' page.");
                     return;
                 }
                 var oAuthIdentity = await user.GenerateUserIdentityAsync(userManager, "JWT");
                 var ticket = new AuthenticationTicket(oAuthIdentity, null);
-                context.Validated(ticket);
+                ctx.Validated(ticket);
             }
             catch (Exception ex)
             {
