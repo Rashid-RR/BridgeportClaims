47344d9ba18f603c75b9d427b940e83a334e7bc8
Jordan Gurney <jordangurney@gmail.com>
2018-03-08 01:35:11 -0700

    Prescriptions and Pharmacies

---
+++ Database/Schema/Tables/dbo.InvoiceIndex.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [dbo].[InvoiceIndex]
+(
+[DocumentID] [int] NOT NULL,
+[InvoiceNumber] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[ModifiedByUserID] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CreatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfInvoiceIndexCreatedOnUTC] DEFAULT (sysutcdatetime()),
+[UpdatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfInvoiceIndexUpdatedOnUTC] DEFAULT (sysutcdatetime()),
+[DataVersion] [timestamp] NOT NULL
+) ON [PRIMARY]
+WITH
+(
+DATA_COMPRESSION = ROW
+)
+GO
+ALTER TABLE [dbo].[InvoiceIndex] ADD CONSTRAINT [pkInvoiceIndex] PRIMARY KEY CLUSTERED  ([DocumentID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[InvoiceIndex] ADD CONSTRAINT [fkInvoiceIndexDocumentIDDocumentDocumentID] FOREIGN KEY ([DocumentID]) REFERENCES [dbo].[Document] ([DocumentID])
+GO
+ALTER TABLE [dbo].[InvoiceIndex] ADD CONSTRAINT [fkInvoiceIndexModifiedByUserIDAspNetUsersID] FOREIGN KEY ([ModifiedByUserID]) REFERENCES [dbo].[AspNetUsers] ([ID])
+GO
