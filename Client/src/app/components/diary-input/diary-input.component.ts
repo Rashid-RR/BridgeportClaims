9c1f8eeccbacd071b388d88cc01277c511580110
Josephat <jogwayi@oosint.com>
2018-03-23 17:18:11 +0300

    references #801, #800, #799

--- Client/src/app/components/diary-input/diary-input.component.ts
+++ Client/src/app/components/diary-input/diary-input.component.ts
@@ -4,7 +4,12 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { DatePipe } from '@angular/common';
 // Services
 import { DiaryService } from '../../services/diary.service';
-declare var $:any;
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/observable/fromEvent';
+declare var $: any;
 
 @Component({
   selector: 'app-diary-input',
@@ -16,22 +21,43 @@ export class DiaryInputComponent implements OnInit, AfterViewInit {
   diaryForm: FormGroup;
   startDate: String;
   endDate: String;
-  open:boolean=true;
-  closed:Boolean=false;
-  submitted:boolean=false;
-  isClosed:Boolean=false;
+  open: boolean = true;
+  closed: Boolean = false;
+  submitted: boolean = false;
+  isClosed: Boolean = false;
+  owner = new FormControl();
+  diaryNote = new FormControl();
+  ownerCtrlSub: Subscription;
+  noteCtrlSub: Subscription;
+  resizeSub: Subscription;
   constructor(
     public ds: DiaryService,
     private dp: DatePipe,
     private toast: ToastsManager,
     private fb: FormBuilder
   ) {
-      
+
   }
 
   ngOnInit() {
+    this.noteCtrlSub = this.diaryNote.valueChanges
+      .debounceTime(1000)
+      .subscribe(newValue => {
+        this.ds.data.diaryNote = newValue || undefined
+        this.search();
+      });
+    this.ownerCtrlSub = this.owner.valueChanges
+      .debounceTime(1000)
+      .subscribe(newValue => {
+        this.ds.data.owner = newValue || undefined
+        this.search();
+      });
   }
 
+  ngOnDestroy() {
+    this.ownerCtrlSub.unsubscribe();
+    this.noteCtrlSub.unsubscribe();
+  }
   ngAfterViewInit() {
     // Date picker
     $('#startDate').datepicker({
@@ -44,38 +70,38 @@ export class DiaryInputComponent implements OnInit, AfterViewInit {
     $('[data-mask]').inputmask();
   }
 
-  setClosed($event,value){
-    if($event.target.checked){
-      this.isClosed=value;
+  setClosed($event, value) {
+    if ($event.target.checked) {
+      this.isClosed = value;
       this.search();
     }
   }
   search() {
     let startDate = this.dp.transform($('#startDate').val(), "MM/dd/yyyy");
     let endDate = this.dp.transform($('#endDate').val(), "MM/dd/yyyy");
-    this.ds.data.closed=this.isClosed;
+    this.ds.data.closed = this.isClosed;
     //if(this.startDate && this.endDate){
-      this.ds.data.startDate = startDate || null
-      this.ds.data.endDate = endDate || null
-      this.ds.search();
-   /*  }else{
-        this.toast.warning("Ensure you select both start date and end date");
-    } */
+    this.ds.data.startDate = startDate || null
+    this.ds.data.endDate = endDate || null
+    this.ds.search();
+    /*  }else{
+         this.toast.warning("Ensure you select both start date and end date");
+     } */
   }
 
-  clearText(){
-    this.ds.owner=null;
-    this.ds.diaryNote=null;
+  clearText() {
+    this.ds.owner = null;
+    this.ds.diaryNote = null;
   }
-  clearDates(){
-    $('#startDate').val('');
-    $('#endDate').val(''); 
-  }
-  reset(){
+  clearDates() {
     $('#startDate').val('');
     $('#endDate').val('');
-    this.ds.owner=null;
-    this.ds.diaryNote=null;
+  }
+  reset() {
+    $('#startDate').val('');
+    $('#endDate').val('');
+    this.ds.owner = null;
+    this.ds.diaryNote = null;
   }
 
 }
