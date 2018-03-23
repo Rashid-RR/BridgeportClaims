5a78f99f526f469f67009c443768ee20604544c3
Jordan Gurney <jgurney2016@hotmail.com>
2018-03-23 16:38:19 -0600

    Carefully committing massive changes.

---
+++ Server/BridgeportClaims.Web/Controllers/CustomActionResults/DisplayFileResult.cs
@@ -0,0 +1,45 @@
+﻿using BridgeportClaims.Common.Extensions;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using c = BridgeportClaims.Common.StringConstants.Constants;
+
+namespace BridgeportClaims.Web.CustomActionResults
+{
+    public class DisplayFileResult : IHttpActionResult
+    {
+        private readonly string _filePath;
+        private readonly string _fileName;
+        private readonly string _contentType;
+
+        public DisplayFileResult(string filePath, string fileName, string contentType = null)
+        {
+            _filePath = filePath.IsNullOrWhiteSpace() ? throw new ArgumentNullException(nameof(filePath)) : filePath;
+            _fileName = fileName.IsNullOrWhiteSpace() ? throw new ArgumentNullException(nameof(fileName)) : fileName;
+            _contentType = contentType;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StreamContent(File.OpenRead(_filePath))
+            };
+            var contentType = _contentType ?? MimeMapping.GetMimeMapping(Path.GetExtension(_filePath));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+            if (ContentDispositionHeaderValue.TryParse(c.ContentDispositionUniqueString + _fileName,
+                out var contentDisposition))
+            {
+                response.Content.Headers.ContentDisposition = contentDisposition;
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
