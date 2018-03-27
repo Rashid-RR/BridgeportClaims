42cf2c05e660f03467c55fe905fbd744769757f2
Cricut <jgurney@cricut.com>
2018-03-27 12:56:13 -0600

    Changes to the Billing Statement.

--- Server/Utilities/BridgeportClaims.Excel/Factories/ExcelFactory.cs
+++ Server/Utilities/BridgeportClaims.Excel/Factories/ExcelFactory.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Drawing;
 using System.Data;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Threading;
+using System.Reflection;
 using BridgeportClaims.Common.Disposable;
 using BridgeportClaims.Common.Extensions;
 using BridgeportClaims.Data.Dtos;
-using DocumentFormat.OpenXml.Presentation;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
 using TableStyles = OfficeOpenXml.Table.TableStyles;
-using Spire.Xls;
 
 namespace BridgeportClaims.Excel.Factories
 {
@@ -51,12 +47,11 @@ namespace BridgeportClaims.Excel.Factories
                 excelWorksheet.Cells[2, 1].Style.Font.Bold = true;
                 excelWorksheet.Cells[3, 1].Value = dto.DateOfBirth.HasValue ? $"DOB {dto.DateOfBirth.Value:M/d/yyyy}" : string.Empty;
                 excelWorksheet.Cells[3, 1].Style.Font.Bold = true;
-                excelWorksheet.Cells["A5"].LoadFromDataTable(dt, true, TableStyles.Medium4);
-                //turn of the filtering
-                var table = excelWorksheet.Tables[dt.TableName];
-                table.ShowFilter = false;
+                excelWorksheet.Cells["A7"].LoadFromDataTable(dt, true);
                 var rowCount = excelWorksheet.Dimension.End.Row;
                 var colCount = excelWorksheet.Dimension.End.Column;
+                var tableHeaders = excelWorksheet.Cells[7, 1, 7, colCount];
+                tableHeaders.Style.Border.Bottom.Style = ExcelBorderStyle.Thick;
                 var allCells = excelWorksheet.Cells[1, 1, rowCount, colCount];
                 allCells.AutoFilter = false;
                 allCells.AutoFitColumns();
@@ -77,20 +72,30 @@ namespace BridgeportClaims.Excel.Factories
                 excelWorksheet.Column(colCount).Style.Numberformat.Format = CurrencyFormat;
                 excelWorksheet.Column(colCount).Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
                 excelWorksheet.Column(colCount).AutoFit();
-                excelWorksheet.Cells[rowCount + 1, colCount - 1].Value = "Total Outstanding";
-                excelWorksheet.Cells[rowCount + 1, colCount - 1].Style.Font.Bold = true;
-                excelWorksheet.Cells[rowCount + 1, colCount].Formula = $"=SUM(I6:I{rowCount})";
-                excelWorksheet.Cells[rowCount + 1, colCount].Calculate();
-                excelWorksheet.Cells[rowCount + 1, colCount].Style.Font.Bold = true;
-                excelWorksheet.Cells[rowCount + 1, colCount].Style.Border.Top.Style = ExcelBorderStyle.Medium;
-                excelWorksheet.Cells[rowCount + 1, colCount].Style.Border.Bottom.Style = ExcelBorderStyle.Double; 
-                excelWorksheet.Cells[rowCount + 1, colCount].Style.Border.Bottom.Style = ExcelBorderStyle.Thick;
-                excelWorksheet.Cells[rowCount + 1, colCount].Style.Border.Top.Color.SetColor(Color.Black);
-                excelWorksheet.Cells[rowCount + 1, colCount].Style.Border.Bottom.Color.SetColor(Color.Black);
+                excelWorksheet.Cells[rowCount + 2, colCount - 1].Value = "Total Outstanding";
+                excelWorksheet.Cells[rowCount + 2, colCount - 1].Style.Font.Bold = true;
+                excelWorksheet.Cells[rowCount + 2, colCount].Formula = $"=SUM(I6:I{rowCount})";
+                excelWorksheet.Cells[rowCount + 2, colCount].Calculate();
+                excelWorksheet.Cells[rowCount + 2, colCount].Style.Font.Bold = true;
+                excelWorksheet.Cells[rowCount + 2, colCount].Style.Border.Top.Style = ExcelBorderStyle.Medium;
+                excelWorksheet.Cells[rowCount + 2, colCount].Style.Border.Bottom.Style = ExcelBorderStyle.Double; 
+                excelWorksheet.Cells[rowCount + 2, colCount].Style.Border.Bottom.Style = ExcelBorderStyle.Thick;
+                excelWorksheet.Cells[rowCount + 2, colCount].Style.Border.Top.Color.SetColor(Color.Black);
+                excelWorksheet.Cells[rowCount + 2, colCount].Style.Border.Bottom.Color.SetColor(Color.Black);
                 excelWorksheet.Cells[1, 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
                 excelWorksheet.Cells[2, 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
                 excelWorksheet.Cells[3, 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
                 var fullFilePath = Path.Combine(Path.GetTempPath(), fileName + ".xlsx");
+                Image img = null;
+                DisposableService.Using(
+                    () => Assembly.GetExecutingAssembly()
+                        .GetManifestResourceStream("BridgeportClaims.Excel.EmbeddedResources.logo.png"),
+                    resourceStream => { img = Image.FromStream(resourceStream); });
+                if (null != img)
+                {
+                    var pic = excelWorksheet.Drawings.AddPicture("logo.png", img);
+                    pic.SetPosition(0, 0, colCount - 3, 10);
+                }
                 return DisposableService.Using(() => File.Create(fullFilePath), stream =>
                 {
                     pck.SaveAs(stream);
