38d1ab2254521288d08ccc3b5461e41bd5028f5e
Jordan Gurney <jgurney2016@hotmail.com>
2018-04-09 01:27:12 -0600

    DB Changes

--- Database/Schema/Data/etl.LatestStagedLakerFileLoaded_Data.sql
+++
@@ -1,3 +0,0 @@
-SET IDENTITY_INSERT [etl].[LatestStagedLakerFileLoaded] ON
-INSERT INTO [etl].[LatestStagedLakerFileLoaded] ([ID], [LastFileNameLoaded]) VALUES (1, 'Billing_Claim_File_20180403.csv')
-SET IDENTITY_INSERT [etl].[LatestStagedLakerFileLoaded] OFF
