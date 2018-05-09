47acc6b28493dfd8e24a5d8bd20ee601af69b408
Jordan Gurney <jgurney2016@hotmail.com>
2018-05-08 21:05:23 -0600

    Fixed date

--- Server/BridgeportClaims.Web/Models/FollowUpDateModel.cs
+++ Server/BridgeportClaims.Web/Models/FollowUpDateModel.cs
@@ -5,6 +5,6 @@ namespace BridgeportClaims.Web.Models
     public sealed class FollowUpDateModel
     {
         public int DiaryId { get; set; }
-        public DateTime FollowUpDate { get; set; }
+        public string FollowUpDate { get; set; }
     }
 }
