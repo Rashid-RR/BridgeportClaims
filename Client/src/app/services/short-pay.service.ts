69054e91471e8a0d3d152011b2a8bc64a20df154
Jordan Gurney <jordangurney@gmail.com>
2018-07-05 19:13:11 -0600

    Formatting fixes, but more importantly, displayed a toast success message from the API.

--- Client/src/app/services/short-pay.service.ts
+++ Client/src/app/services/short-pay.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http-service';
-import { SortColumnInfo } from "../directives/table-sort.directive";
+import { SortColumnInfo } from '../directives/table-sort.directive';
 import { Router, NavigationEnd } from '@angular/router';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 export interface ShortPay {
-    lastName: string,
-    firstName: string,
-    rxNumber: number,
-    rxDate: Date,
-    claimNumber: number,
-    billedAmount: any,
-    amountPaid: number,
-    prescriptionStatus: string,
-    prescriptionPaymentId?: number
+    lastName: string;
+    firstName: string;
+    rxNumber: number;
+    rxDate: Date;
+    claimNumber: number;
+    billedAmount: any;
+    amountPaid: number;
+    prescriptionStatus: string;
+    prescriptionPaymentId?: number;
 }
 
 @Injectable()
 export class ShortPayService {
 
-    loading: boolean = false;
-    deleting: boolean = false; 
+    loading = false;
+    deleting = false;
     goToPage: any = '';
     routes: string[] = [];
     shortpay: ShortPay[] = [];
@@ -28,8 +28,8 @@ export class ShortPayService {
     totalRowCount: number;
     constructor(private router: Router, private toast: ToastsManager, private http: HttpService) {
         this.data = {
-            sort: "RxNumber",
-            sortDirection: "DESC",
+            sort: 'RxNumber',
+            sortDirection: 'DESC',
             page: 1,
             pageSize: 30
         }
@@ -39,7 +39,7 @@ export class ShortPayService {
             this.toast.warning('Please populate at least one search field.');
         } else {
             this.loading = true;
-            let data = JSON.parse(JSON.stringify(this.data)); //copy data instead of memory referencing
+            const data = JSON.parse(JSON.stringify(this.data)); // copy data instead of memory referencing
 
             if (next) {
                 data.page++;
@@ -57,7 +57,7 @@ export class ShortPayService {
                 if (next) {
                     this.data.page++;
                 }
-                if (prev && this.data.page != data.page) {
+                if (prev && this.data.page !== data.page) {
                     this.data.page--;
                 }
                 if (page) {
@@ -65,21 +65,23 @@ export class ShortPayService {
                 }
             }, err => {
                 this.loading = false;
-                this.shortpay=[{}as any]
-            })
+                this.shortpay = [{}as any];
+            });
         }
     }
+
     removeShortpay(id: number = undefined) {
         this.loading = true;
-        this.http.removeShortPay({ prescriptionPaymentId: id }).single().map(r => r.json()).subscribe(r => {
+        this.http.removeShortPay({ prescriptionPaymentId: id }).single().map(r => r.json()).subscribe(res => {
+            const result = res.json();
             this.loading = false;
-            this.toast.success(`Shortpay with id ${id} was successfully removed`);
+            this.toast.success(result.message);
             this.fetchShortpayReport();
         }, err => {
             this.loading = false;
-            let error = err.json();
-            this.toast.error(error.Message || error.message)            
-        })
+            const error = err.json();
+            this.toast.error(error.Message || error.message);
+        });
     }
     onSortColumn(info: SortColumnInfo) {
         this.data.isDefaultSort = false;
@@ -102,7 +104,8 @@ export class ShortPayService {
         return this.shortpay.length > 1 ? ((this.data.page - 1) * this.data.pageSize) + 1 : null;
     }
     get pageEnd() {
-        return this.shortpay.length > 1 ? (this.data.pageSize > this.shortpay.length ? ((this.data.page - 1) * this.data.pageSize) + this.shortpay.length : (this.data.page) * this.data.pageSize) : null;
+        return this.shortpay.length > 1 ? (this.data.pageSize > this.shortpay.length ? ((this.data.page - 1) * this.data.pageSize)
+        + this.shortpay.length : (this.data.page) * this.data.pageSize) : null;
     }
 
     get end(): Boolean {
