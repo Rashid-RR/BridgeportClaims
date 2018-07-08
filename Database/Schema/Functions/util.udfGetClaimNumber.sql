665367c266e9b02930051b90cd8569bd9699e045
Jordan Gurney <jordangurney@gmail.com>
2018-07-07 23:22:25 -0600

    Various utility objects.

---
+++ Database/Schema/Functions/util.udfGetClaimNumber.sql
@@ -0,0 +1,14 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE FUNCTION [util].[udfGetClaimNumber](@ClaimID INT)
+RETURNS VARCHAR(255)
+AS BEGIN
+	RETURN
+	(
+		SELECT c.ClaimNumber FROM dbo.Claim AS c WHERE c.ClaimID = @ClaimID
+	)
+END
+GO
