aef07d15d186d577112b2a918f7b4eee325d047f
Josephat <jogwayi@oosint.com>
2018-07-16 17:10:45 +0300

    Banner on our Testing site

--- Client/src/app/layouts/header/header.component.ts
+++ Client/src/app/layouts/header/header.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { EventsService } from "../../services/events-service";
 import { ProfileManager } from "../../services/profile-manager";
 import { HttpService } from "../../services/http-service";
 import { Router } from "@angular/router";
-import { Subscription } from 'rxjs/Subscription';
-import {AuthGuard} from "../../services/auth.guard"
-import {LocalStorageService} from 'ng2-webstorage';
-import {ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { DOCUMENT } from '@angular/platform-browser';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
-declare var jQuery:any
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -17,40 +14,42 @@ declare var jQuery:any
 export class HeaderComponent implements OnInit {
 
   date: number;
-  disableLinks = false;  
+  disableLinks = false;
   constructor(
     private http: HttpService,
     private router: Router,
     public eventservice: EventsService,
     public profileManager: ProfileManager,
-    private guard:AuthGuard,
-    private toast:ToastsManager,
-    private localSt:LocalStorageService
+    private toast: ToastsManager,
+    @Inject(DOCUMENT) private document
   ) { }
-  
+
   ngOnInit() {
     this.date = Date.now();
-    this.eventservice.on("disable-links",(status:boolean)=>{
-        this.disableLinks = status;
-    });    
+    this.eventservice.on("disable-links", (status: boolean) => {
+      this.disableLinks = status;
+    });
   }
 
-  clearCache(){
-    this.http.clearCache().subscribe(res=>{
-        this.toast.success(res.message);
-    },err=>{
+  clearCache() {
+    this.http.clearCache().subscribe(res => {
+      this.toast.success(res.message);
+    }, err => {
       this.toast.error(err.message);
     })
   }
-  get allowed():Boolean{
-    return (this.profileManager.profile.roles && (this.profileManager.profile.roles instanceof Array) && this.profileManager.profile.roles.indexOf('Admin')>-1)
+  get isTestDomain() {
+    return this.document.location.hostname == 'bridgeportclaims-testing.azurewebsites.net';
   }
-  sidebarToggle(){
-    this.eventservice.broadcast("sidebarOpen", true);    
+  get allowed(): Boolean {
+    return (this.profileManager.profile.roles && (this.profileManager.profile.roles instanceof Array) && this.profileManager.profile.roles.indexOf('Admin') > -1)
+  }
+  sidebarToggle() {
+    this.eventservice.broadcast("sidebarOpen", true);
   }
 
   logout() {
-    if(!this.disableLinks){
+    if (!this.disableLinks) {
       this.eventservice.broadcast("logout", true);
       this.profileManager.profile = undefined;
       localStorage.removeItem('user');
