b6f859809b6b23ed5a04a8129d6e26d273da9d5e
Jordan Gurney <jordangurney@gmail.com>
2018-07-22 16:26:04 -0600

    proc optimization

--- Database/Schema/Stored Procedures/dbo.uspGetClaimsWithPrescriptionDetails.sql
+++ Database/Schema/Stored Procedures/dbo.uspGetClaimsWithPrescriptionDetails.sql
@@ -9,58 +9,48 @@ GO
 	Sample Execute:
 					EXEC [dbo].[uspGetClaimsWithPrescriptionDetails] '8,9,10,11'
 */
-CREATE PROC [dbo].[uspGetClaimsWithPrescriptionDetails]
-(
-	@ClaimIDs VARCHAR(4000)
-)
-AS BEGIN
-	SET NOCOUNT ON;
-	SET TRAN ISOLATION LEVEL READ COMMITTED; -- SNAPSHOT
-	SET XACT_ABORT ON;
-	BEGIN TRY
-		BEGIN TRAN;
-		
-		SELECT [pr].[PrescriptionID] PrescriptionId
-			 , [c].[ClaimID] ClaimId
-			 , [c].[ClaimNumber]
-			 , PatientName = [p].[FirstName] + ' ' + [p].[LastName]
-			 , RxDate = CAST(FORMAT([pr].[DateFilled], 'M/d/yyyy') AS DATE)
-			 , [pr].[RxNumber]
-			 , [pr].[LabelName]
-			 , InvoicedAmount = [pr].[BilledAmount]
-			 , Outstanding = [pr].[BilledAmount] - ISNULL([ot].[AmountPaid], 0)
-			 , Payor = [py].[GroupName]
-			 , pr.IsReversed
-		FROM   [dbo].[Claim] AS [c]
-			   INNER JOIN STRING_SPLIT(@ClaimIDs, ',') AS [ss] ON [ss].[value] = [c].[ClaimID]
-			   INNER JOIN [dbo].[Payor] AS [py] ON [py].[PayorID] = [c].[PayorID]
-			   INNER JOIN [dbo].[Patient] AS p ON [p].[PatientID] = [c].[PatientID]
-			   INNER JOIN [dbo].[Prescription] AS [pr] ON [pr].[ClaimID] = [c].[ClaimID]
-			   LEFT JOIN [dbo].[Invoice] AS [i] ON [i].[InvoiceID] = [pr].[InvoiceID]
-			   OUTER APPLY (   SELECT AmountPaid = SUM([ipm].[AmountPaid])
-							   FROM   [dbo].[PrescriptionPayment] AS [ipm]
-							   WHERE  [ipm].[PrescriptionID] = [pr].[PrescriptionID]
-						   ) AS ot
-		IF @@TRANCOUNT > 0
-			COMMIT
-	END TRY
-	BEGIN CATCH
-		IF @@TRANCOUNT > 0
-			ROLLBACK
+CREATE PROC [dbo].[uspGetClaimsWithPrescriptionDetails] (@ClaimIDs VARCHAR(4000))
+AS
+    BEGIN
+        SET NOCOUNT ON;
+        SET XACT_ABORT ON;
+        BEGIN TRY
+            BEGIN TRAN;
 
-        DECLARE @ErrSeverity INT = ERROR_SEVERITY()
-            , @ErrState INT = ERROR_STATE()
-            , @ErrProc NVARCHAR(MAX) = ERROR_PROCEDURE()
-            , @ErrLine INT = ERROR_LINE()
-            , @ErrMsg NVARCHAR(MAX) = ERROR_MESSAGE()
+            SELECT  PrescriptionId = pr.PrescriptionID
+                   ,ClaimId = c.ClaimID
+                   ,c.ClaimNumber
+                   ,PatientName = p.FirstName + ' ' + p.LastName
+                   ,RxDate = CAST(FORMAT(pr.DateFilled, 'M/d/yyyy') AS DATE)
+                   ,pr.RxNumber
+                   ,pr.LabelName
+                   ,InvoicedAmount = pr.BilledAmount
+                   ,Outstanding = pr.BilledAmount - ISNULL((   SELECT   SUM(ipm.AmountPaid)
+                                                               FROM dbo.PrescriptionPayment AS [ipm]
+                                                               WHERE   ipm.PrescriptionID = [pr].[PrescriptionID]), 0)
+                   ,Payor = py.GroupName
+                   ,pr.IsReversed
+            FROM    dbo.Claim AS [c]
+                    INNER JOIN STRING_SPLIT(@ClaimIDs, ',') AS [ss] ON ss.value = c.ClaimID
+                    INNER JOIN dbo.Payor AS [py] ON py.PayorID = c.PayorID
+                    INNER JOIN dbo.Patient AS p ON p.PatientID = c.PatientID
+                    INNER JOIN dbo.Prescription AS [pr] ON pr.ClaimID = c.ClaimID
+                    LEFT JOIN dbo.Invoice AS [i] ON i.InvoiceID = pr.InvoiceID;
 
-        RAISERROR(N'%s (line %d): %s',	-- Message text w formatting
-			@ErrSeverity,		-- Severity
-			@ErrState,			-- State
-			@ErrProc,			-- First argument (string)
-			@ErrLine,			-- Second argument (int)
-			@ErrMsg)			-- First argument (string)
-	END CATCH
-END
+            IF (@@TRANCOUNT > 0 )
+				COMMIT;
+        END TRY
+        BEGIN CATCH
+            IF (@@TRANCOUNT > 0)
+				ROLLBACK;
 
+            DECLARE @ErrSeverity INT = ERROR_SEVERITY()
+                   ,@ErrState INT = ERROR_STATE()
+                   ,@ErrProc NVARCHAR(MAX) = ERROR_PROCEDURE()
+                   ,@ErrLine INT = ERROR_LINE()
+                   ,@ErrMsg NVARCHAR(MAX) = ERROR_MESSAGE()
+
+            RAISERROR(N'%s (line %d): %s', @ErrSeverity, @ErrState, @ErrProc, @ErrLine, @ErrMsg);
+        END CATCH
+    END
 GO
