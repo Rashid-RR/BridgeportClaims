e3f79a18efd19528b7a2fb8efb4d33bb4fe1170a
Josephat <jogwayi@oosint.com>
2018-08-06 15:00:42 +0300

    references #904

--- Client/src/app/components/invoice-search-modal/invoice-search-modal.component.ts
+++ Client/src/app/components/invoice-search-modal/invoice-search-modal.component.ts
@@ -122,7 +122,7 @@ export class InvoiceSearchComponent implements OnInit, AfterViewInit {
   validateAfter($event) {
     if ((this.amount && this.amount.toString().length > 1 && this.amount.toString().lastIndexOf("-") > 0) ||
       (this.amount && this.amount.toString().length > 1 && this.amount.toString().lastIndexOf(".") > this.amount.toString().indexOf(".")) ||
-      (this.amount && this.amount && this.amount.toString().lastIndexOf(".") > -1 && this.amount.toString().length - this.amount.toString().lastIndexOf(".") > 3) ||
+      //(this.amount && this.amount && this.amount.toString().lastIndexOf(".") > -1 && this.amount.toString().length - this.amount.toString().lastIndexOf(".") > 3) ||
       (this.amount && this.amount.toString().length == 1 && this.amount.toString().lastIndexOf(".") > 0)
     ) {
       let num = String(this.amount);
