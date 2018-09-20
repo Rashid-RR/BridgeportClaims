61a7cf60d3c2788b227fa8e9b4c85e4c646c70b7
Jordan Gurney <jgurney@cricut.com>
2018-09-20 07:54:58 -0600

    Dapper Repo.

---
+++ Server/DAL/BridgeportClaims.Data/Repositories/DapperRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using BridgeportClaims.Common.Disposable;
+using Dapper;
+using cs = BridgeportClaims.Common.Config.ConfigService;
+
+namespace BridgeportClaims.Data.Repositories
+{
+    public class DapperRepository
+    {
+        private const int Timeout = 1800;
+
+        protected IEnumerable<TModel> ExecuteAndReturnCollection<TModel>(string sp, DynamicParameters parameters) =>
+            DisposableService.Using(() => new SqlConnection(cs.GetDbConnStr()), conn =>
+            {
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                return conn.Query<TModel>(sp, parameters, commandType: CommandType.StoredProcedure,
+                    commandTimeout: Timeout);
+            });
+
+        protected TModel ExecuteAndReturnSingleOrDefault<TModel>(string sp, DynamicParameters parameters) =>
+            DisposableService.Using(() => new SqlConnection(cs.GetDbConnStr()), conn =>
+            {
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                return conn.Query<TModel>(sp,
+                    parameters, commandType: CommandType.StoredProcedure,
+                    commandTimeout: Timeout).SingleOrDefault();
+            });
+
+        protected void ExecuteVoid(string sp, DynamicParameters parameters) =>
+            DisposableService.Using(() => new SqlConnection(cs.GetDbConnStr()), conn =>
+            {
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                conn.Execute(sp, parameters, commandType: CommandType.StoredProcedure, commandTimeout: Timeout);
+            });
+    }
+}
