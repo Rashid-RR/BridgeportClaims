23de48300d526fd9fcb3156c342e534470115f8f
Jordan Gurney <jgurney@cricut.com>
2018-09-19 20:19:50 -0600

    Changed project to use TopShelf!

---
+++ WindowsService/BridgeportClaims.FileWatcherService.Host/BridgeportClaims.FileWatcherService.Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BridgeportClaims.Business.Logging;
+using NLog;
+
+namespace BridgeportClaims.FileWatcherService.Host
+{
+    public class Program
+    {
+        private static readonly LoggingService LoggingService = LoggingService.Instance;
+        private static readonly Logger Logger = LoggingService.Logger;
+
+        public static void Main(string[] args)
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += FileWatcherServiceUnhandledExceptionHandler;
+
+        }
+
+        private static void FileWatcherServiceUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            var ex = (Exception)args.ExceptionObject;
+            Logger.Fatal(ex, "A fatal exception was thrown.");
+            throw ex;
+        }
+    }
+}
