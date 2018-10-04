e2a5a69c6ea2e5dc49fd859c74f69fe46bb558e8
Jordan Gurney <jgurney@cricut.com>
2018-10-04 09:14:24 -0600

    checking in

--- Server/DAL/BridgeportClaims.Data/Dtos/ClaimNoteTypeDto.cs
+++ Server/DAL/BridgeportClaims.Data/Dtos/ClaimNoteTypeDto.cs
@@ -1,13 +1,17 @@
-﻿using SQLinq;
+﻿using ProtoBuf;
+using SQLinq;
 
 namespace BridgeportClaims.Data.Dtos
 {
     [SQLinqTable("ClaimNoteType")]
+    [ProtoContract]
     public sealed class ClaimNoteTypeDto
     {
         [SQLinqColumn("ClaimNoteTypeID")]
+        [ProtoMember(1)]
         public int ClaimNoteTypeId { get; set; }
         [SQLinqColumn("TypeName")]
+        [ProtoMember(2)]
         public string TypeName { get; set; }
     }
 }
