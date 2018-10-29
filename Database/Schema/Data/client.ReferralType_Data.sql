2ca0d4d66739f188fe6e1481964579770eca6e1d
Jordan Gurney <jgurney@cricut.com>
2018-10-28 21:08:17 -0600

    Ref data

---
+++ Database/Schema/Data/client.ReferralType_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [client].[ReferralType] ON
+INSERT INTO [client].[ReferralType] ([ReferralTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (1, 'Insurance Carrier', 'INCA', '2018-10-25 00:07:06.3545976', '2018-10-25 00:07:06.3545976')
+INSERT INTO [client].[ReferralType] ([ReferralTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (2, 'Physician', 'PHYS', '2018-10-25 00:07:06.3545976', '2018-10-25 00:07:06.3545976')
+INSERT INTO [client].[ReferralType] ([ReferralTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (3, 'Attorney', 'ATTY', '2018-10-25 00:07:06.3545976', '2018-10-25 00:07:06.3545976')
+SET IDENTITY_INSERT [client].[ReferralType] OFF
