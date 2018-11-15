cfc566acd304efa715a6c86cf6d59b6d1d3a0a23
Jordan Gurney <jgurney@cricut.com>
2018-11-15 00:56:57 -0700

    Fixed collections assignments.

--- Server/BridgeportClaims.Web/Controllers/PrescriptionsController.cs
+++ Server/BridgeportClaims.Web/Controllers/PrescriptionsController.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Data;
 using System.Linq;
 using System.Web.Http;
-using System.Threading.Tasks;
 using System.Collections.Generic;
 using BridgeportClaims.Business.BillingStatement;
 using BridgeportClaims.Common.Constants;
@@ -57,37 +56,6 @@ namespace BridgeportClaims.Web.Controllers
                 var users = _prescriptionsDataProvider.Value.GetActiveUsers()?.OrderBy(x => x.FirstName)
                     .ThenBy(x => x.LastName);
                 return Ok(users);
-            }
-            catch (Exception ex)
-            {
-                Logger.Value.Error(ex);
-                return Content(HttpStatusCode.NotAcceptable, new { message = ex.Message });
-            }
-        }
-
-        [HttpPost]
-        [Route("assign-users-to-payors")]
-        public async Task<IHttpActionResult> AssignUsersToPayors(CollectionAssignmentModel model)
-        {
-            try
-            {
-                if (null == model)
-                    throw new ArgumentNullException(nameof(model));
-                if (null == model.UserId)
-                    throw new ArgumentNullException(nameof(model.UserId));
-                if (null == model.PayorIds)
-                    throw new ArgumentNullException(nameof(model.PayorIds));
-                var user = await AppUserManager.FindByIdAsync(model.UserId).ConfigureAwait(false);
-                if (default(int) == model.PayorIds.Count)
-                {
-                    Logger.Value.Info($"We are removing all associations (if there are any), from {user.FullName}");
-                }
-                IList<CarrierDto> carrierDtos =
-                    model.PayorIds.Select(item => new CarrierDto { PayorID = item }).ToList();
-                var dt = carrierDtos.ToFixedDataTable();
-                var modifiedByUserId = User.Identity.GetUserId();
-                _collectionAssignmentProvider.Value.MergeCollectionAssignments(model.UserId, modifiedByUserId, dt);
-                return Ok(new {message = $"{user.FullName} was associated to the selected carriers successfully."});
             }
             catch (Exception ex)
             {
