ba9cf58e92ce75acd81a74568313951af0fcecec
Jordan Gurney <jgurney@cricut.com>
2018-11-22 21:45:12 -0700

    Reporting.

---
+++ Server/DAL/BridgeportClaims.Data/Dtos/CollectionsBonusResultsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BridgeportClaims.Data.Dtos
+{
+    public sealed class CollectionsBonusResultsDto
+    {
+        [Required]
+        public string PatientName { get; set; }
+        [Required]
+        public DateTime DatePosted { get; set; }
+        [Required]
+        public decimal AmountPaid { get; set; }
+        [Required]
+        public decimal BonusAmount { get; set; }
+    }
+}
