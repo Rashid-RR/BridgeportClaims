1fe8618487aa7394b1df4003ba6d64de29a79692
Jordan Gurney <jgurney@cricut.com>
2018-11-22 22:02:15 -0700

    Adding Claim ID to Collections Bonus report.

--- Server/DAL/BridgeportClaims.Data/Dtos/CollectionsBonusResultsDto.cs
+++ Server/DAL/BridgeportClaims.Data/Dtos/CollectionsBonusResultsDto.cs
@@ -6,6 +6,8 @@ namespace BridgeportClaims.Data.Dtos
     public sealed class CollectionsBonusResultsDto
     {
         [Required]
+        public int ClaimId { get; set; }
+        [Required]
         public string PatientName { get; set; }
         [Required]
         public DateTime DatePosted { get; set; }
