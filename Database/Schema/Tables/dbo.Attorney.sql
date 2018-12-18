7c6f0911a15971b6b6578a835d484638c27b48ec
Jordan Gurney <jgurney@cricut.com>
2018-12-18 08:24:37 -0700

    Att

---
+++ Database/Schema/Tables/dbo.Attorney.sql
@@ -0,0 +1,33 @@
+CREATE TABLE [dbo].[Attorney]
+(
+[AttorneyID] [int] NOT NULL IDENTITY(1, 1),
+[AttorneyName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Address1] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Address2] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[City] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[StateID] [int] NULL,
+[PostalCode] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PhoneNumber] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FaxNumber] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ModifiedByUserID] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[CreatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfAttorneyCreatedOnUTC] DEFAULT (sysutcdatetime()),
+[UpdatedOnUTC] [datetime2] NOT NULL CONSTRAINT [dfAttorneyUpdatedOnUTC] DEFAULT (sysutcdatetime()),
+[DataVersion] [timestamp] NOT NULL
+) ON [PRIMARY]
+WITH
+(
+DATA_COMPRESSION = ROW
+)
+GO
+ALTER TABLE [dbo].[Attorney] ADD CONSTRAINT [pkAttorney] PRIMARY KEY CLUSTERED  ([AttorneyID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = ROW) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [idxAttorneyAttorneyName] ON [dbo].[Attorney] ([AttorneyName]) WITH (FILLFACTOR=90, DATA_COMPRESSION = PAGE) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [idxAttorneyModifiedByUserID] ON [dbo].[Attorney] ([ModifiedByUserID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = PAGE) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [idxAttorneyStateID] ON [dbo].[Attorney] ([StateID]) WITH (FILLFACTOR=90, DATA_COMPRESSION = PAGE) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Attorney] ADD CONSTRAINT [fkAttorneyModifiedByUserIDAspNetUsersID] FOREIGN KEY ([ModifiedByUserID]) REFERENCES [dbo].[AspNetUsers] ([ID])
+GO
+ALTER TABLE [dbo].[Attorney] ADD CONSTRAINT [fkAttorneyStateIDUsStateStateID] FOREIGN KEY ([StateID]) REFERENCES [dbo].[UsState] ([StateID])
+GO
