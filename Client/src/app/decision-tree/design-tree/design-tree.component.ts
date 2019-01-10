9549aaba9242ac54563c678bb7b0d38e70b77c09
Josephat <jogwayi@oosint.com>
2019-01-10 14:36:37 +0300

    adjust tree with as one expands through tree

--- Client/src/app/decision-tree/design-tree/design-tree.component.ts
+++ Client/src/app/decision-tree/design-tree/design-tree.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
 import * as d3 from 'd3';
 import { ITreeNode } from '../tree-node';
 import { HttpService } from '../../services/http-service';
+declare var $: any;
 
 @Component({
   selector: 'app-design-tree',
@@ -11,11 +12,13 @@ import { HttpService } from '../../services/http-service';
 export class DesignTreeComponent implements OnInit, AfterViewInit {
 
   decisionTreeSVG: any;
-  margin = { top: 10, right: 120, bottom: 10, left: 40 };
+  margin = { top: 20, right: 90, bottom: 30, left: 30 };
   get width(): number {
-    return (this.treeDepth+1) * this.dy;
+    return (this.treeDepth + 1) + 60;
   };
-  height = 300 - this.margin.top - this.margin.bottom;
+  duration: number = 750
+  i: number = 0
+  height = 500 - this.margin.top - this.margin.bottom;
   treeData: ITreeNode =
     {
       "nodeName": "Top Level",
@@ -25,28 +28,40 @@ export class DesignTreeComponent implements OnInit, AfterViewInit {
     };
   dx: number = 35;
   dy: number = 120;
-  tree = data => {
-    const root = d3.hierarchy(data);
-    this.depth = root.height;
-    root.dx = this.dx;
-    root.dy = this.dy;//this.width / (root.height + 1);
-    return d3.tree().nodeSize([root.dx, root.dy])(root);
-  }
+  svg: any;
+  tree = d3.tree().size([this.height, this.width]);
+  root: any;
   loading: boolean = false;
   rootTreeId: number = 2;
   depth: number = 930;
   get treeDepth() {
-    return this.depth;
+    return this.depth || 0;
   }
   constructor(private http: HttpService) { }
-
+  collapse(d) {
+    if (d.children) {
+      d._children = d.children
+      d._children.forEach(c => this.collapse(c));
+      d.children = null
+    }
+  }
   ngAfterViewInit() {
     this.loading = true;
     this.http.getTree({ rootTreeId: this.rootTreeId })
       .subscribe((result: any) => {
         this.loading = false;
         this.treeData = result;
-        this.chart();
+        this.root = d3.hierarchy(this.treeData, (d) => { return d.children; });
+        this.root.x0 = this.height / 2;
+        this.root.y0 = 20;
+        this.root.y = 20;
+        this.svg = d3.select('svg')
+          .style("width", this.width)
+          .style("height", this.height)
+          .attr("transform", "translate("
+            + this.margin.left + "," + this.margin.top + ")");
+        this.root.children.forEach(c => this.collapse(c));
+        this.update(this.root);
       }, err => {
         this.loading = false;
         try {
@@ -58,159 +73,131 @@ export class DesignTreeComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
   }
-
-  /*  update(source) {
-     const duration = d3.event && d3.event.altKey ? 2500 : 250;
-     const nodes = this.root.descendants().reverse();
-     const links = this.root.links();
-     // Compute the new tree layout.
-     this.tree(this.root);
- 
-     let left = this.root;
-     let right = this.root;
-     this.root.eachBefore(node => {
-       if (node.x < left.x) left = node;
-       if (node.x > right.x) right = node;
-     });
- 
-     console.log(right.x, right.y)
-     const height = right.x - left.x + this.margin.top + this.margin.bottom;
- 
-     const transition = this.decisionTreeSVG.transition()
-       .duration(duration)
-       .attr("height", height)
-       .attr("viewBox", [-this.margin.left, left.x - this.margin.top, this.width, height])
-       .tween("resize", () => () => this.decisionTreeSVG.dispatch("toggle"));
- 
-     // Update the nodes…
-     const node = this.gNode.selectAll("g")
-       .data(nodes, d => d.id);
- 
-     // Enter any new nodes at the parent's previous position.
-     const nodeEnter = node.enter().append("g")
-       .attr("transform", d => `translate(${source.y0},${source.x0})`)
-       .attr("fill-opacity", 0)
-       .attr("stroke-opacity", 0)
-       .on("click", d => {
-         d.children = d.children ? null : d._children;
-         this.update(d);
-       });
- 
-     nodeEnter.append("circle")
-       .attr("r", 2.5)
-       .attr("fill", d => d._children ? "#555" : "#999");
- 
-     nodeEnter.append("text")
-       .attr("dy", "0.31em")
-       .attr("x", d => d._children ? -6 : 6)
-       .attr("text-anchor", d => d._children ? "end" : "start")
-       .text(d => d.data.name)
-       .clone(true).lower()
-       .attr("stroke-linejoin", "round")
-       .attr("stroke-width", 3)
-       .attr("stroke", "white");
- 
-     // Transition nodes to their new position.
-     node.merge(nodeEnter).transition(transition)
-       .attr("transform", d => `translate(${d.y},${d.x})`)
-       .attr("fill-opacity", 1)
-       .attr("stroke-opacity", 1);
- 
-     // Transition exiting nodes to the parent's new position.
-     node.exit().transition(transition).remove()
-       .attr("transform", d => `translate(${source.y},${source.x})`)
-       .attr("fill-opacity", 0)
-       .attr("stroke-opacity", 0);
- 
-     // Update the links…
-     const link = this.gLink.selectAll("path")
-       .data(links, d => d.target.id);
- 
-     // Enter any new links at the parent's previous position.
-     const linkEnter = link.enter().append("path")
-       .attr("d", d => {
-         const o = { x: source.x0, y: source.y0 };
-         return this.diagonal({ source: o, target: o });
-       });
- 
-     // Transition links to their new position.
-     link.merge(linkEnter).transition(transition)
-       .attr("d", this.diagonal);
- 
-     // Transition exiting nodes to the parent's new position.
-     link.exit().transition(transition).remove()
-       .attr("d", d => {
-         const o = { x: source.x, y: source.y };
-         return this.diagonal({ source: o, target: o });
-       });
- 
-     // Stash the old positions for transition.
-     this.root.eachBefore(d => {
-       d.x0 = d.x;
-       d.y0 = d.y;
-     });
-     return this.decisionTreeSVG.node();
-   } */
-  chart = () => {
-    const root = this.tree(this.treeData);
-
-    let x0 = Infinity;
-    let x1 = -x0;
-    root.each(d => {
-      if (d.x > x1) x1 = d.x;
-      if (d.x < x0) x0 = d.x;
-    });
-
-    const svg = d3.select('svg')
-      .style("width", this.width)
-      .style("height", this.height);
-
-    const g = svg.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
-
-    const link = g.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
-      .selectAll("path")
-      .data(root.links())
-      .enter().append("path")
-      .attr("d", d3.linkHorizontal()
-        .x(d => d.y)
-        .y(d => d.x));
-
-    const node = g.append("g")
-      .attr("class", "pointer")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-width", 3)
-      .selectAll("g")
-      .data(root.descendants().reverse())
-      .enter().append("g")
-      .attr("transform", d => `translate(${d.y},${d.x})`)
-      .on("click", this.nodeClick);;
-
-    node.append("circle")
-    .attr("class", "pointer")
-      .attr("fill", d => d.children ? "#555" : "#999")
-      .attr("r", 10);
-
-    node.append("text")
-      .attr("dy", "-0.8em")
-      .attr("x", d => d.children ? 0 :13)
-      /* .attr("dy", "0.31em")
-      .attr("x", d => d.children ? -6 : 6) */
-      .attr("text-anchor", d => d.children ? "end" : "start")
-      .text(d => d.data.nodeName)
-      .clone(true).lower()
-      .attr("stroke", "white");
-
-    return svg.node();
+  diagonal(s, d) {
+    return `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`;
   }
-  nodeClick(d) {
-    console.log(d);
+  update(source) {
+
+    // Assigns the x and y position for the nodes
+    var treeData = this.tree(this.root);
+
+    // Compute the new tree layout.
+    var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+    // Normalize for fixed-depth.
+    nodes.forEach((d) => { d.y = d.depth * 180 + 70 });
+
+    // ****************** Nodes section ***************************
+
+    // Update the nodes...
+    var node = this.svg.selectAll('g.node')
+      .data(nodes, (d) => { return d.id || (d.id = ++this.i); });
+
+    // Enter any new modes at the parent's previous position.
+    var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", (d) => {
+        return "translate(" + source.y0 + "," + source.x0 + ")";
+      })
+      .on('click', (n) => this.click(n));
+
+    // Add Circle for the nodes
+    nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 1e-6)
+      .style("fill", (d) => {
+        return d._children ? "lightsteelblue" : "#fff";
+      });
+
+    // Add labels for the nodes
+    nodeEnter.append('text')
+      .attr("dy", ".35em")
+      .attr("x", (d) => {
+        return d.children || d._children ? -13 : 13;
+      })
+      .attr("text-anchor", (d) => {
+        return d.children || d._children ? "end" : "start";
+      })
+      .text((d) => { return d.data.nodeName; });
+
+    // UPDATE
+    var nodeUpdate = nodeEnter.merge(node);
+
+    // Transition to the proper position for the node
+    nodeUpdate.transition()
+      .duration(this.duration)      
+      .attr("transform", (d) => {
+        this.depth = (180*d.data.treeLevel);
+        $("svg:last-child").css("width", this.width+"px");
+        return "translate(" + d.y + "," + d.x + ")";
+      });
+
+    // Update the node attributes and style
+    nodeUpdate.select('circle.node')
+      .attr('r', 10)
+      .style("fill", (d) => {
+        return d._children ? "lightsteelblue" : "#fff";
+      })
+      .attr('cursor', 'pointer');
+
+
+    // Remove any exiting nodes
+    var nodeExit = node.exit().transition()
+      .duration(this.duration)
+      .attr("transform", (d) => {
+        return "translate(" + source.y + "," + source.x + ")";
+      })
+      .remove();
+
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle')
+      .attr('r', 1e-6);
+
+    // On exit reduce the opacity of text labels
+    nodeExit.select('text')
+      .style('fill-opacity', 1e-6);
+
+    // ****************** links section ***************************
+
+    // Update the links...
+    var link = this.svg.selectAll('path.link')
+      .data(links, (d) => { return d.id; });
+
+    // Enter any new links at the parent's previous position.
+    var linkEnter = link.enter().insert('path', "g")
+      .attr("class", "link")
+      .attr('d', (d) => {
+        var o = { x: source.x0, y: source.y0 }
+        return this.diagonal(o, o)
+      });
+
+    // UPDATE
+    var linkUpdate = linkEnter.merge(link);
+
+    // Transition back to the parent element position
+    linkUpdate.transition()
+      .duration(this.duration)
+      .attr('d', (d) => { return this.diagonal(d, d.parent) });
+
+    // Remove any exiting links
+    link.exit().transition()
+      .duration(this.duration)
+      .attr('d', (d) => {
+        var o = { x: source.x, y: source.y }
+        return this.diagonal(o, o)
+      })
+      .remove();
+
+    // Store the old positions for transition.
+    nodes.forEach((d) => {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });   
+  }
+  click(d) {
     if (d.children) {
       d._children = d.children;
       d.children = null;
@@ -218,7 +205,8 @@ export class DesignTreeComponent implements OnInit, AfterViewInit {
       d.children = d._children;
       d._children = null;
     }
-   // update(d);
+    this.update(d);
   }
+
 
 }
