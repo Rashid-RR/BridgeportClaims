b1380471a8981cb4c197d19aab10b051d692b871
Josephat <jogwayi@oosint.com>
2019-01-22 11:31:12 +0300

    Sample material MAT_AUTOCOMPLETE_DEFAULT_OPTIONS import

--- Client/src/app/material/material-components.module.ts
+++ Client/src/app/material/material-components.module.ts
@@ -1,16 +1,19 @@
-import {MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule} from '@angular/material';
-import {NgModule} from '@angular/core';
+import { MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule, MAT_AUTOCOMPLETE_SCROLL_STRATEGY } from '@angular/material';
+import { MAT_AUTOCOMPLETE_DEFAULT_OPTIONS } from '@angular/material/autocomplete';
+
+import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule],
-
-
-
+  providers: [
+    { provide: MAT_AUTOCOMPLETE_DEFAULT_OPTIONS, useValue: { autoActiveFirstOption: false } }
+  ],
   exports: [MatButtonModule,
     MatFormFieldModule,
+
     MatSelectModule,
     MatInputModule],
 })
