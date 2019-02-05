1aa0f7428d1563890f6f7ce41f95cd751d7c5735
Jordan Gurney <jgurney@cricut.com>
2019-02-04 17:32:35 -0700

    API Changes.

--- Server/DAL/BridgeportClaims.Data/DataProviders/Payors/PayorsDataProvider.cs
+++ Server/DAL/BridgeportClaims.Data/DataProviders/Payors/PayorsDataProvider.cs
@@ -129,7 +129,8 @@ namespace BridgeportClaims.Data.DataProviders.Payors
                 ps.Add("@Contact", contact, DbType.AnsiString, size: 255);
                 ps.Add("@LetterName", letterName, DbType.AnsiString, size: 255);
                 ps.Add("@ModifiedByUserID", modifiedByUserId, DbType.String, size: 128);
-                return conn.Query<PayorResultDto>(sp, ps, commandType: CommandType.StoredProcedure)?.SingleOrDefault();
+                var query = conn.Query<PayorResultDto>(sp, ps, commandType: CommandType.StoredProcedure)?.SingleOrDefault();
+                return query;
             });
     }
 }
