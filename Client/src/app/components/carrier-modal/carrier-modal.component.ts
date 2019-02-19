fa29a43787cedf19f77c83ac8a1e6b47ba4679b6
Jordan Gurney <jordangurney@gmail.com>
2019-02-19 14:47:58 -0700

    Fixed some modal display issues.

--- Client/src/app/components/carrier-modal/carrier-modal.component.ts
+++ Client/src/app/components/carrier-modal/carrier-modal.component.ts
@@ -31,19 +31,6 @@ export class CarrierModalComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  convertPhoneNumber(num: any) {
-    let convertedNumber;
-    if (num.length === 10) {
-      convertedNumber = '(' + num.substring(0, 3) + ') ' + num.substring(3, 6) + '-' + num.substring(6, 10);
-      return convertedNumber;
-
-    } else if (num.length === 11) {
-      convertedNumber = num.substring(0, 1) + '-(' + num.substring(1, 4) + ') ' + num.substring(4, 7) + '-' + num.substring(7, 11);
-      return convertedNumber;
-
-    }
-  }
-
   showReference(payorId: any) {
     this.dialogRef.close();
     this.router.navigate(['main/references'], {queryParams: {payorId: payorId}});
