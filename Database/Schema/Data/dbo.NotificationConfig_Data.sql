0925d5eb945f5388b8eac10c9a396f47d33866f5
Jordan Gurney <jgurney2016@hotmail.com>
2019-03-15 12:32:29 -0600

    Data Edits.

--- Database/Schema/Data/dbo.NotificationConfig_Data.sql
+++ Database/Schema/Data/dbo.NotificationConfig_Data.sql
@@ -1,6 +1,4 @@
 SET IDENTITY_INSERT [dbo].[NotificationConfig] ON
-INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (2, 2, CAST(1 AS int), 'INT', '2019-03-14', '2019-03-14 11:34:03.3523630', '2019-03-14 11:36:58.8453769')
-SET IDENTITY_INSERT [dbo].[NotificationConfig] OFF
-SET IDENTITY_INSERT [dbo].[NotificationConfig] ON
-INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (1, 1, CAST(216 AS int), 'INT', '2019-03-14', '2018-03-14 08:28:00.4431864', '2019-03-14 21:54:44.4430965')
+INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (1, 1, CAST(217 AS int), 'INT', '2019-03-15', '2018-03-14 08:28:00.4431864', '2019-03-15 09:34:18.3443660')
+INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (2, 2, CAST(0 AS int), 'INT', '2019-03-15', '2019-03-15 06:47:16.5145451', '2019-03-15 06:47:16.5145451')
 SET IDENTITY_INSERT [dbo].[NotificationConfig] OFF
