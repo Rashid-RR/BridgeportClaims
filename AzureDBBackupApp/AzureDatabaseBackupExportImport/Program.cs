24603e9d4ea06f894953a07a5fde8ee9ab06679f
Jordan Gurney <jgurney2016@hotmail.com>
2019-03-29 13:28:09 -0600

    Some changes to the PowerShell project.

--- AzureDBBackupApp/AzureDatabaseBackupExportImport/Program.cs
+++ AzureDBBackupApp/AzureDatabaseBackupExportImport/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AzureDatabaseBackupExportImport
 {
@@ -12,27 +8,31 @@ namespace AzureDatabaseBackupExportImport
     {
         public static void Main(string[] args)
         {
-            var scriptFile = @"C:\Development\PowerShell\ExportSqlServerBacpac.ps1";
-            var runSpaceConfiguration = RunspaceConfiguration.Create();
-
-            var runSpace = RunspaceFactory.CreateRunspace(runSpaceConfiguration);
-            runSpace.Open();
-
-            using (var scriptInvoker = new RunspaceInvoke(runSpace))
+            try
             {
-                scriptInvoker.Invoke("Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass");
-                var pipeline = runSpace.CreatePipeline();
-
-                //Here's how you add a new script with arguments
-                var myCommand = new Command(scriptFile);
-                /*var testParam = new CommandParameter("key", "value");
-                myCommand.Parameters.Add(testParam);*/
-
-                pipeline.Commands.Add(myCommand);
-
-                // Execute PowerShell script
-                var results = pipeline.Invoke();
+                var scriptFile = @"C:\Development\PowerShell\ImportSqlServerBacpac.ps1";
+                var runSpaceConfiguration = RunspaceConfiguration.Create();
+                var runSpace = RunspaceFactory.CreateRunspace(runSpaceConfiguration);
+                runSpace.Open();
+                using (var scriptInvoker = new RunspaceInvoke(runSpace))
+                {
+                    scriptInvoker.Invoke("Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass");
+                    var pipeline = runSpace.CreatePipeline();
+                    //Here's how you add a new script with arguments
+                    var myCommand = new Command(scriptFile);
+                    /*var testParam = new CommandParameter("key", "value");
+                    myCommand.Parameters.Add(testParam);*/
+                    pipeline.Commands.Add(myCommand);
+                    // Execute PowerShell script
+                    var results = pipeline.Invoke();
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
         }
     }
 }
