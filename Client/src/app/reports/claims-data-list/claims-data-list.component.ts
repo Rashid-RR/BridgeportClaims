c26b808fd55aa48f4c7c237db0ffc6bcdc2b4fa8
Jordan Gurney <jgurney2016@hotmail.com>
2019-06-04 11:25:34 -0600

    Modified project.

--- Client/src/app/reports/claims-data-list/claims-data-list.component.ts
+++ Client/src/app/reports/claims-data-list/claims-data-list.component.ts
@@ -8,23 +8,55 @@ import { QueryBuilderService } from '../../services/query-builder.service';
   styleUrls: ['./claims-data-list.component.css']
 })
 export class ClaimsDataListComponent implements OnInit {
+  public defaultColDef: any;
+  public rowSelection: string;
+  public rowGroupPanelShow: string;
+  public pivotPanelShow: string;
+  private gridApi;
+  private gridColumnApi;
+
   @ViewChild('agGrid') agGrid: AgGridNg2;
   columnDefs = [
-    { headerName: 'Make', field: 'make', sortable: true, filter: true, rowGroup: true, checkboxSelection: true },
-    { headerName: 'Model', field: 'model', sortable: true, filter: true },
-    { headerName: 'Price', field: 'price', sortable: true, filter: true }
+    // { headerName: 'ClaimId', field: 'claimId', sortable: true, filter: true, checkboxSelection: true, rowDrag: true },
+    // { headerName: 'PrescriptionId', field: 'prescriptionId', sortable: true, filter: true },
+    // { headerName: 'PrescriptionPaymentId', field: 'prescriptionPaymentId', sortable: true, filter: true },
+    { headerName: 'Pharmacy', field: 'pharmacy', sortable: true, filter: true, rowDrag: true },
+    { headerName: 'State', field: 'stateCode', sortable: true, filter: true },
+    { headerName: 'Submitted', field: 'dateSubmitted', sortable: true, filter: true },
+    { headerName: 'Billed', field: 'billed', sortable: true, filter: true },
+    { headerName: 'Payable', field: 'payable', sortable: true, filter: true },
+    { headerName: 'Collected', field: 'collected', sortable: true, filter: true },
+    { headerName: 'Prescriber', field: 'prescriber', sortable: true, filter: true },
+    { headerName: 'Patient Last', field: 'patientLast', sortable: true, filter: true },
+    { headerName: 'Patient First', field: 'patientFirst', sortable: true, filter: true },
+    { headerName: 'Claim #', field: 'claimNumber', sortable: true, filter: true },
+    { headerName: 'Attorney Managed', field: 'isAttorneyManaged', sortable: true, filter: true },
+    { headerName: 'Attorney Name', field: 'attorneyName', sortable: true, filter: true }
   ];
   autoGroupColumnDef = {
-    headerName: 'Model',
-    field: 'model',
+    headerName: 'GroupName',
+    field: 'groupName',
     cellRenderer: 'agGroupCellRenderer',
     cellRendererParams: {
         checkbox: true
     }
-};
+  };
 
   rowData: any;
-  constructor(private queryBuilderSvc: QueryBuilderService) {}
+  constructor(private queryBuilderSvc: QueryBuilderService) {
+    this.defaultColDef = {
+      editable: true,
+      enableRowGroup: true,
+      enablePivot: true,
+      enableValue: true,
+      sortable: true,
+      resizable: true,
+      filter: true
+    };
+    this.rowSelection = 'multiple';
+    this.rowGroupPanelShow = 'always';
+    this.pivotPanelShow = 'always';
+  }
 
   ngOnInit(): void {
     this.rowData = this.queryBuilderSvc.fetchQueryBuilderReport();
@@ -35,5 +67,11 @@ export class ClaimsDataListComponent implements OnInit {
     const selectedData = selectedNodes.map(node => node.data);
     const selectedDataStringPresentation = selectedData.map(node => node.make + ' ' + node.model).join(', ');
     console.log(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.gridApi.api.sizeColumnsToFit();
   }
 }
