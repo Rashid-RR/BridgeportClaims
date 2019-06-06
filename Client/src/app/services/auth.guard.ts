42717b875fb01421c3c21fef95bf717312eb04bd
Jordan Gurney <jgurney2016@hotmail.com>
2019-06-06 16:43:36 -0600

    Permissions

--- Client/src/app/services/auth.guard.ts
+++ Client/src/app/services/auth.guard.ts
@@ -59,9 +59,12 @@ export class AuthGuard implements CanActivate, CanActivateChild, Resolve<UserPro
       } else if (route.path === 'users' || route.path === 'fileupload') {
         const allowed = (us.roles && (us.roles instanceof Array) && us.roles.indexOf('Admin') > -1);
         return of(allowed);
-      } else if (state.url.indexOf('/main/reports') > -1 && (state.url.indexOf('/main/reports/skipped-payment')
-        === -1 && state.url.indexOf('/main/reports/shortpay')
-        === -1 && state.url.indexOf('/main/reports/list') === -1)) {
+      } else if (state.url.indexOf('/main/reports') > -1 &&
+        (state.url.indexOf('/main/reports/skipped-payment') === -1 &&
+        state.url.indexOf('/main/reports/shortpay') === -1 &&
+        state.url.indexOf('/main/reports/claims-data') === -1 &&
+        state.url.indexOf('/main/reports/address-edit') === -1 &&
+        state.url.indexOf('/main/reports/list') === -1)) {
         const allowed = state.url.indexOf('/main/reports/collection-bonus') > -1 ? (us.roles && us.roles instanceof Array
           && us.roles.indexOf('User') > -1) : (us.roles && us.roles instanceof Array && us.roles.indexOf('Admin') > -1);
         return of(allowed);
