889f010c8c3fd6258d3fbc9a2689a4a14bb93b4f
Jordan Gurney <jordangurney@gmail.com>
2019-06-30 16:28:13 -0600

    Sftp Changes

--- ConsoleApp/LakerFileImporter/Business/LakerFileProcessor.cs
+++ ConsoleApp/LakerFileImporter/Business/LakerFileProcessor.cs
@@ -16,7 +16,7 @@ namespace LakerFileImporter.Business
 {
     internal class LakerFileProcessor
     {
-        private static readonly Logger Logger = LoggingService.Instance.Logger;
+        private static readonly Lazy<Logger> Logger = new Lazy<Logger>(() => LoggingService.Instance.Logger);
 
         /// <summary>
         /// Boolean returns whether or not the process was even necessary (and successful)
@@ -29,7 +29,7 @@ namespace LakerFileImporter.Business
                 var methodName = MethodBase.GetCurrentMethod().Name;
                 var now = DateTime.Now.ToString("G");
                 if (cs.AppIsInDebugMode)
-                    Logger.Info($"Entered the {methodName} method on {now}");
+                    Logger.Value.Info($"Entered the {methodName} method on {now}");
                 var importFileProvider = new ImportFileProvider();
                 // Query the database.
                 var dbFiles = importFileProvider.GetImportFileDtos();
@@ -37,16 +37,17 @@ namespace LakerFileImporter.Business
                 var lastProcessedFileFromDatabase = dbFiles?.Where(p => p.Processed).OrderByDescending(x => x.LakerFileDate).FirstOrDefault();
                 // Ok, so now we have the latest file that has been processed in the database.
                 if (null != lastProcessedFileFromDatabase && cs.AppIsInDebugMode)
-                    Logger.Info($"The last processed Laker file in the Database was {lastProcessedFileFromDatabase.FileName}. Recording this from method {methodName} on {now}.");
-               
+                {
+                    Logger.Value.Info($"The last processed Laker file in the Database was {lastProcessedFileFromDatabase.FileName}. Recording this from method {methodName} on {now}.");
+                }
                 // Now, let's prepare any necessary folders in the local directory, in preparation for an SFTP operation.
-                var ioHelper = new IoHelper();
-                var methodSucceeded = ioHelper.CreateMonthAndYearFolderIfNecessary();
+                var methodSucceeded = new IoHelper().CreateMonthAndYearFolderIfNecessary();
                 if (!methodSucceeded)
                 {
                     if (cs.AppIsInDebugMode)
-                        Logger.Error(
-                            "Something wrong happened with the creation of the month and year folder. Please read the previous exception message.");
+                    {
+                        Logger.Value.Error("Something wrong happened with the creation of the month and year folder. Please read the previous exception message.");
+                    }
                     return LakerFileProcessResult.MonthYearFolderCouldNotBeCreatedInLocalDirectory;
                 }
                 // Now, work in the SFTP to download any new files (regardless of whether or not we need them at this point). -- For testing purposes, this should have the option of being skipped.
@@ -59,37 +60,44 @@ namespace LakerFileImporter.Business
                 if (!processSftp)
                 {
                     if (cs.AppIsInDebugMode)
-                        Logger.Info(
-                            "We are NOT doing any SFTP processing because the flag in the App.Config told us not to.");
+                    {
+                        Logger.Value.Info("We are NOT doing any SFTP processing because the flag in the App.Config told us not to.");
+                    }
                 }
                 else
                 {
                     // Do any necessary SFTP downloading...
                     if (cs.AppIsInDebugMode)
                     {
-                        Logger.Info("Ok, beginning SFTP operation(s)...");
+                        Logger.Value.Info("Ok, beginning SFTP operation(s)...");
                     }
-                    // First download Laker File(s)
                     var proxyProvider = new SftpProxyProvider();
+                    // First download Laker File(s)
                     proxyProvider.ProcessLakerSftpOperation();
                     // Next, download any Envision File(s)
                     proxyProvider.ProcessEnvisionSftpOperation();
                 }
                 // Now that we've downloaded all SFTP files, we can simply traverse the local directory for the latest file (as we were originally doing) -
-                var newestFileInLocalDirectory = ioHelper.BrowseDirectoryToLocateFile();
+                var newestFileInLocalDirectory = new IoHelper().BrowseDirectoryToLocateFile();
                 if (null == newestFileInLocalDirectory)
                 {
                     if (cs.AppIsInDebugMode)
-                        Logger.Info("No file was found in the local directory to process. You might want to make sure we're pointing to the right place. " +
-                                    $"This was recorded in the {methodName} method on {now}.");
+                    {
+                        Logger.Value.Info("No file was found in the local directory to process. You might want to make sure we're pointing to the right place. " +
+                            $"This was recorded in the {methodName} method on {now}.");
+                    }
                     return LakerFileProcessResult.NoFilesFoundInFileDirectory;
                 }
+
                 if (cs.AppIsInDebugMode)
-                    Logger.Info($"The newest file found in the local directory is {newestFileInLocalDirectory.FullFileName}. This was recorded in the {methodName} method on {now}.");
-                var newLakerFileNeededForProcessing =
-                    null == lastProcessedFileFromDatabase || newestFileInLocalDirectory.LakerFileDate > lastProcessedFileFromDatabase.LakerFileDate;
+                {
+                    Logger.Value.Info($"The newest file found in the local directory is {newestFileInLocalDirectory.FullFileName}. This was recorded in the {methodName} method on {now}.");
+                }
+                var newLakerFileNeededForProcessing = null == lastProcessedFileFromDatabase || newestFileInLocalDirectory.LakerFileDate > lastProcessedFileFromDatabase.LakerFileDate;
                 if (!newLakerFileNeededForProcessing)
+                {
                     return LakerFileProcessResult.NoLakerFileProcessingNecessary;
+                }
                 // Upload and process Laker File on Server.
                 var apiClient = new ApiClient();
                 var bytes = File.ReadAllBytes(newestFileInLocalDirectory.FullFileName);
@@ -97,15 +105,15 @@ namespace LakerFileImporter.Business
                 var newLakerFileName = newestFileInLocalDirectory.FileName;
                 var succeededUpload = await apiClient.UploadFileToApiAsync(bytes, newLakerFileName, token);
                 if (!succeededUpload)
+                {
                     return LakerFileProcessResult.LakerFileFailedToUpload;
+                }
                 var succeededProcessing = await apiClient.ProcessLakerFileToApiAsync(newLakerFileName, token);
-                return !succeededProcessing
-                    ? LakerFileProcessResult.LakerFileFailedToProcess
-                    : LakerFileProcessResult.LakerFileProcessStartedSuccessfully;
+                return !succeededProcessing ? LakerFileProcessResult.LakerFileFailedToProcess : LakerFileProcessResult.LakerFileProcessStartedSuccessfully;
             }
             catch (Exception ex)
             {
-                Logger.Error(ex);
+                Logger.Value.Error(ex);
                 throw;
             }
         }
