652e7476cccfc1ef07a4aeb76ebb279c6f96813d
Jordan Gurney <jordangurney@gmail.com>
2019-07-01 02:31:47 -0600

    Auto stash before rebase of "origin/sidebar-redesign"

--- Database/Schema/Data/dbo.NotificationConfig_Data.sql
+++ Database/Schema/Data/dbo.NotificationConfig_Data.sql
@@ -1,4 +1,4 @@
 SET IDENTITY_INSERT [dbo].[NotificationConfig] ON
-INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (1, 1, CAST(217 AS int), 'INT', '2019-03-15', '2018-03-14 08:28:00.4431864', '2019-03-15 09:34:18.3443660')
-INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (2, 2, CAST(0 AS int), 'INT', '2019-03-15', '2019-03-15 06:47:16.5145451', '2019-03-15 06:47:16.5145451')
+INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (1, 1, CAST(229 AS int), 'INT', '2019-06-30', '2018-03-14 08:28:00.4431864', '2019-06-30 09:49:57.6021933')
+INSERT INTO [dbo].[NotificationConfig] ([NotificationConfigID], [NotificationTypeID], [NotificationValue], [SQLVariantDataType], [EffectiveDate], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (2, 2, CAST(2 AS int), 'INT', '2019-06-04', '2019-03-15 06:47:16.5145451', '2019-06-04 09:44:36.6582841')
 SET IDENTITY_INSERT [dbo].[NotificationConfig] OFF
