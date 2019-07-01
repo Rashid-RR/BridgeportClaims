fb3ce4bc4a6c1fd0575d5845afb2f539136b637d
Jordan Gurney <jordangurney@gmail.com>
2019-07-01 02:24:43 -0600

    Committing DB Objects.

--- Database/Schema/Data/util.ImportFileType_Data.sql
+++ Database/Schema/Data/util.ImportFileType_Data.sql
@@ -1,8 +1,8 @@
 SET IDENTITY_INSERT [util].[ImportFileType] ON
+INSERT INTO [util].[ImportFileType] ([ImportFileTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (4, 'Envision Import', 'EI', '2019-06-30 21:50:46.0169727', '2019-06-30 21:50:46.0169727')
+SET IDENTITY_INSERT [util].[ImportFileType] OFF
+SET IDENTITY_INSERT [util].[ImportFileType] ON
 INSERT INTO [util].[ImportFileType] ([ImportFileTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (1, 'Laker Import', 'LI', '2017-08-11 09:34:25.5102613', '2017-08-11 09:34:25.5102613')
 INSERT INTO [util].[ImportFileType] ([ImportFileTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (2, 'Payment Import', 'PI', '2017-08-11 09:34:25.5102613', '2017-08-11 09:34:25.5102613')
 INSERT INTO [util].[ImportFileType] ([ImportFileTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (3, 'Other', 'OT', '2017-09-09 10:22:57.0948870', '2017-09-09 10:22:57.0948870')
-SET IDENTITY_INSERT [util].[ImportFileType] OFF
-SET IDENTITY_INSERT [util].[ImportFileType] ON
-INSERT INTO [util].[ImportFileType] ([ImportFileTypeID], [TypeName], [Code], [CreatedOnUTC], [UpdatedOnUTC]) VALUES (4, 'Envision Import', 'EI', '2019-06-30 21:51:32.1819239', '2019-06-30 21:51:32.1819239')
 SET IDENTITY_INSERT [util].[ImportFileType] OFF
