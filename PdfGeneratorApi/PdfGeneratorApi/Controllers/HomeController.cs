a9a4d5a093187fc6366cb730be9ec3a3a4034d6b
Jordan Gurney <jordangurney@gmail.com>
2019-07-31 22:32:22 -0600

    Adding new API

---
+++ PdfGeneratorApi/PdfGeneratorApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace PdfGeneratorApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("shit")]
+        public IHttpActionResult GetShit()
+        {
+            return Ok("FUKC");
+        }
+    }
+}
